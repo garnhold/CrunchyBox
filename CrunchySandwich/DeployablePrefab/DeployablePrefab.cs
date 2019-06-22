d7ba6a7ab809960a44bb96619c0b9c3f545d0180
garnhold <50562729+garnhold@users.noreply.github.com>
2019-06-22 14:21:28 -0700

    Added DeployablePrefab

---
+++ CrunchySandwich/DeployablePrefab/DeployablePrefab.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+using CrunchyDough;
+using CrunchyBun;
+
+namespace CrunchySandwich
+{
+    public abstract class DeployablePrefab : MonoBehaviour
+    {
+    }
+
+    public abstract class DeployablePrefab<T> : DeployablePrefab where T : DeployablePrefab<T>
+    {
+        static private T PREFAB;
+        static public T DeployPrefabInstance()
+        {
+            if (PREFAB == null)
+            {
+                PREFAB = (T)DeployablePrefabManager.GetInstance<DeployablePrefabManager>()
+                    .GetPrefabs()
+                    .FindFirst(p => p.Is<T>());
+            }
+
+            return PREFAB;
+        }
+    }
+}
---
+++ CrunchySandwich/DeployablePrefab/DeployablePrefabManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+using CrunchyDough;
+using CrunchyBun;
+
+namespace CrunchySandwich
+{
+    public class DeployablePrefabManager : Subsystem
+    {
+        [SerializeField]private DeployablePrefab[] prefabs;
+
+        public void Initialize(IEnumerable<DeployablePrefab> p)
+        {
+            prefabs = p.ToArray();
+        }
+        public void Initialize(params DeployablePrefab[] p)
+        {
+            Initialize((IEnumerable<DeployablePrefab>)p);
+        }
+
+        public IEnumerable<DeployablePrefab> GetPrefabs()
+        {
+            return prefabs;
+        }
+    }
+}
