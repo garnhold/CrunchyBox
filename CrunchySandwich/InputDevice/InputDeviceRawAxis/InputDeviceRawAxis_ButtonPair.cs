27ec105c78d5dd107abd418f8b95928789957117
Garrett Arnhold <garrett@arnholdsystems.com>
2019-10-26 15:31:37 -0700

    Added InputDeviceRawAxis_ButtonPair

---
+++ CrunchySandwich/InputDevice/InputDeviceRawAxis/InputDeviceRawAxis_ButtonPair.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+using CrunchyDough;
+
+namespace CrunchySandwich
+{
+    public class InputDeviceRawAxis_ButtonPair : InputDeviceRawAxis
+    {
+        private InputDeviceRawButton negative_button;
+        private InputDeviceRawButton positive_button;
+
+        public InputDeviceRawAxis_ButtonPair(InputDeviceRawButton nb, InputDeviceRawButton pb)
+        {
+            negative_button = nb;
+            positive_button = pb;
+        }
+
+        public override float GetValue()
+        {
+            if (negative_button.IsButtonDown())
+            {
+                if (positive_button.IsButtonDown() == false)
+                    return -1.0f;
+            }
+            else
+            {
+                if (positive_button.IsButtonDown())
+                    return 1.0f;
+            }
+
+            return 0.0f;
+        }
+    }
+}
