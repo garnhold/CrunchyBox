30d8310adb3fecb59215bf690e624d3dd546549f
Garrett Arnhold <garrett@arnholdsystems.com>
2019-11-26 01:20:20 -0800

    Added Text Gizmo and Misc Mesh Stuff.

---
+++ CrunchySandwich/Extensions/Gizmos/GizmosExtensions_Text.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+using UnityEngine;
+
+using CrunchyDough;
+
+namespace CrunchySandwich
+{
+    static public partial class GizmosExtensions
+    {
+        static public void DrawText(Vector3 position, Quaternion rotation, string text, float size, TextAnchor text_anchor, Color color)
+        {
+            TextGenerationSettings settings = new TextGenerationSettings();
+
+            settings.font = GUI.skin.font;
+            settings.color = color;
+            settings.fontSize = 32;
+            settings.textAnchor = text_anchor;
+            settings.fontStyle = FontStyle.Normal;
+            settings.verticalOverflow = VerticalWrapMode.Overflow;
+            settings.horizontalOverflow = HorizontalWrapMode.Overflow;
+            settings.generateOutOfBounds = true;
+            settings.scaleFactor = 1.0f;
+
+            settings.font.material.SetPass(0);
+
+            settings.CreateAndUseMesh(text, delegate(Mesh mesh) {
+                Graphics.DrawMeshNow(
+                    mesh,
+                    Matrix4x4.TRS(
+                        position,
+                        rotation,
+                        ((Vector2.one / settings.fontSize) * size).GetSpacar(1.0f)
+                    )
+                );
+            });
+        }
+        static public void DrawText(Vector3 position, string text, float size, TextAnchor text_anchor, Color color)
+        {
+            DrawText(
+                position,
+                Quaternion.LookRotation(Camera.current.transform.forward),
+                text,
+                size,
+                text_anchor,
+                color
+            );
+
+        }
+    }
+}
---
+++ CrunchySandwich/Extensions/TextGenerationSettings/TextGenerationSettingsExtensions_Mesh.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+using CrunchyDough;
+using CrunchyBun;
+
+namespace CrunchySandwich
+{
+    static public class TextGenerationSettingsExtensions_Mesh
+    {
+        static public Mesh CreateMesh(this TextGenerationSettings item, string text)
+        {
+            TextGenerator generator = new TextGenerator();
+
+            generator.Populate(text, item);
+            return generator.CreateMesh();
+        }
+
+        static public void CreateAndUseMesh(this TextGenerationSettings item, string text, Process<Mesh> process)
+        {
+            Mesh mesh = item.CreateMesh(text);
+
+                process(mesh);
+
+            Mesh.DestroyImmediate(mesh);
+        }
+    }
+}
