0bb7662d18dadcf39d0233639acc747cdfbfb0ef
Garrett Arnhold <garrett@arnholdsystems.com>
2019-11-26 01:19:49 -0800

    Added AIMorpher. Don't Know If I like It.

---
+++ CrunchyStrudel/AIMorpher/AIMorpher.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using CrunchyDough;
+using CrunchyNoodle;
+using CrunchyBun;
+
+namespace CrunchyStrudel
+{
+    public class AIMorpher
+    {
+        private AIMorpherNode current_node;
+
+        public AIMorpher(AIMorpherNode n)
+        {
+            current_node = n;
+            current_node.IfNotNull(z => z.Resume());
+        }
+
+        public void Update(float step)
+        {
+            current_node = current_node.Transition(
+                current_node.IfNotNull(n => n.Update(step)),
+                n => n.Resume(),
+                n => n.Suspend()
+            );
+        }
+    }
+}
---
+++ CrunchyStrudel/AIMorpher/Node/AIMorpherNode.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using CrunchyDough;
+using CrunchyNoodle;
+using CrunchyBun;
+
+namespace CrunchyStrudel
+{
+    public abstract class AIMorpherNode
+    {
+        private float current_interest;
+        private List<AIMorpherNode> nodes;
+
+        protected abstract float UpkeepInternal();
+
+        protected virtual void ResumeInternal() { }
+        protected virtual void SuspendInternal() { }
+        protected virtual void UpdateInternal(float interest) { }
+
+        private double Upkeep(float step)
+        {
+            current_interest += (UpkeepInternal() - 0.5f) * step;
+
+            current_interest = current_interest.BindPercent();
+            return current_interest;
+        }
+
+        public AIMorpherNode()
+        {
+            nodes = new List<AIMorpherNode>();
+        }
+
+        public void Initialize(IEnumerable<AIMorpherNode> n)
+        {
+            nodes.Set(n);
+        }
+
+        public void Resume()
+        {
+            ResumeInternal();
+        }
+
+        public void Suspend()
+        {
+            SuspendInternal();
+        }
+
+        public AIMorpherNode Update(float step)
+        {
+            double other_interest;
+            AIMorpherNode other_node = nodes.FindHighestRated(n => n.Upkeep(step), out other_interest);
+
+            if (other_interest >= Upkeep(step) * 2.0f)
+                return other_node;
+
+            UpdateInternal(current_interest);
+            return this;
+        }
+
+        public double GetCurrentInterest()
+        {
+            return current_interest;
+        }
+    }
+}
