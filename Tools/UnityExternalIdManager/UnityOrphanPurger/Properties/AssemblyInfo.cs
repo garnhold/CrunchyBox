b1634e5325ba15252cdda32801a69c747fafeead
Garrett Arnhold <garrett@arnholdsystems.com>
2019-12-13 15:39:55 -0800

    Moved OrphanPurging From CrunchySandwichBag to a Standalone Tool

---
+++ Tools/UnityExternalIdManager/UnityOrphanPurger/Properties/AssemblyInfo.cs
@@ -0,0 +1,39 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+
+using Crunchy.Noodle;
+
+// Information about this assembly is defined by the following attributes. 
+// Change them to the values specific to your project.
+
+[assembly: Crunchy.Dough.ProgramInfo("UnityOrphanPurger")]
+
+[assembly: AssemblyTitle("UnityOrphanPurger")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: InspectedAssembly]
+[assembly: InspectAssemblys_ByType(
+    typeof(Crunchy.Dough.ThisAssembly),
+    typeof(Crunchy.Salt.ThisAssembly),
+    typeof(Crunchy.Noodle.ThisAssembly),
+    typeof(Crunchy.Sodium.ThisAssembly),
+    typeof(Crunchy.Recipe.ThisAssembly)
+)]
+
+// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
+// The form "{Major}.{Minor}.*" will automatically update the build and revision,
+// and "{Major}.{Minor}.{Build}.*" will update just the revision.
+
+[assembly: AssemblyVersion("1.0.*")]
+
+// The following attributes are used to specify the signing key for the assembly, 
+// if desired. See the Mono documentation for more information about signing.
+
+//[assembly: AssemblyDelaySign(false)]
+//[assembly: AssemblyKeyFile("")]
