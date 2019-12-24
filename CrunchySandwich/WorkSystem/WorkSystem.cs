b2dbc4020d3b0dea67f790f178211b0e72a288ae
Garrett Arnhold <garrett@arnholdsystems.com>
2019-12-24 11:37:19 -0800

    Added ScheduleNamed to WorkSystem

--- CrunchySandwich/WorkSystem/WorkSystem.cs
+++ CrunchySandwich/WorkSystem/WorkSystem.cs
@@ -14,22 +14,30 @@ namespace Crunchy.Sandwich
         [SerializeFieldEX]private Duration target_frame_length = Duration.Hertz(60.0f);
 
         private LazyScheduler scheduler;
+        private DecayTimeSet<Tuple<UnityEngine.Object, string>> registered_named_work_ids;
+
+        private void UpdateInternal()
+        {
+            scheduler.Update(target_frame_length);
+            registered_named_work_ids.Decay();
+        }
 
         public WorkSystem()
         {
             scheduler = new LazyScheduler(new BinSchedule(4096, Duration.Seconds(0.050f)));
+            registered_named_work_ids = new DecayTimeSet<Tuple<UnityEngine.Object, string>>();
         }
 
         public override void Update()
         {
             if (Application.isPlaying)
-                scheduler.Update(target_frame_length);
+                UpdateInternal();
         }
 
         public override void UpdateInEditor()
         {
             if (Application.isPlaying == false)
-                scheduler.Update(target_frame_length);
+                UpdateInternal();
         }
 
         public void Schedule(UnityEngine.Object requester, long delay, Process process)
@@ -45,6 +53,18 @@ namespace Crunchy.Sandwich
                     Debug.LogException(ex);
                 }
             });
+        }
+
+        public void ScheduleNamed(UnityEngine.Object requester, string name, long delay, Process process)
+        {
+            Tuple<UnityEngine.Object, string> id = new Tuple<UnityEngine.Object, string>(requester, name);
+
+            if(registered_named_work_ids.Add(id, delay, false))
+            {
+                Schedule(requester, delay, delegate () {
+                    process();
+                });
+            }
         }
     }
 }
