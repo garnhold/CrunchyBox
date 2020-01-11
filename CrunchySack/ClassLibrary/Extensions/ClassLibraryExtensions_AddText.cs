02cadac5fb6b9bda8094f381d4b2783dd03676ad
Garrett Arnhold <garrett@arnholdsystems.com>
2020-01-10 21:46:11 -0800

    Added a AddFragment and AddClass Extension Method to CrunchySack
    Added these dudes to simplify the creation of fragments/classes within cs code.

---
+++ CrunchySack/ClassLibrary/Extensions/ClassLibraryExtensions_AddText.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Crunchy.Sack
+{
+    using Dough;
+    using Salt;
+    using Noodle;
+    
+    static public class ClassLibraryExtensions_AddText
+    {
+        static public void AddClass(this ClassLibrary item, Type type, string layout, string text)
+        {
+            item.AddClass(
+                new CmlEntry_Class_ClassDefinition(
+                    type,
+                    layout,
+                    CmlClassDefinition.DOMify(text)
+                )
+            );
+        }
+    }
+}
---
+++ CrunchySack/FragmentLibrary/Extensions/FragmentLibraryExtensions_AddText.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Crunchy.Sack
+{
+    using Dough;
+    using Salt;
+    using Noodle;
+    
+    static public class FragmentLibraryExtensions_AddText
+    {
+        static public void AddFragment(this FragmentLibrary item, string name, string text)
+        {
+            item.AddFragment(
+                new CmlEntry_Fragment_FragmentDefinition(
+                    name,
+                    CmlFragmentDefinition.DOMify(text)
+                )
+            );
+        }
+    }
+}
