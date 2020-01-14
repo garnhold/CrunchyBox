195bdb7b2dc15cff702aa976246d68bfb75239ac
Garrett Arnhold <garrett@arnholdsystems.com>
2020-01-14 13:27:20 -0800

    Added TextField + NumericField to CrunchySack_WinForms

---
+++ CrunchySack_WinForms/Basic/BasicWinFormsEngineInitilizer/BasicWinFormsEngineInitilizer_Field.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+using System.Windows.Forms;
+
+namespace Crunchy.Sack.WinForms
+{
+    using Dough;
+    using Sack;
+    using WinCommon;
+    
+    [BasicWinFormsEngineInitilizer]
+    static public class BasicWinFormsEngineInitilizer_Field
+    {
+        [BasicWinFormsEngineInitilizer]
+        static public void Initilize(WinFormsEngine engine)
+        {
+            engine.AddPublicPropertyAttributeLinksForType<TextBox>();
+            engine.Add(
+                WinFormsInstancers.Simple("TextField", () => new TextBox())
+            );
+
+            engine.AddPublicPropertyAttributeLinksForType<NumericUpDown>();
+            engine.Add(
+                WinFormsInstancers.Simple("NumericField", () => new NumericUpDown())
+            );
+        }
+    }
+}
