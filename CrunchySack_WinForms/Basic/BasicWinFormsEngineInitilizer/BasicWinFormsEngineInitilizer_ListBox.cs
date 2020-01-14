6a3fa162ad49570b8bfe1d0c81df4e792665dd25
Garrett Arnhold <garrett@arnholdsystems.com>
2020-01-14 13:48:16 -0800

    Added Slider + ListBox to CrunchySack_WinForms

---
+++ CrunchySack_WinForms/Basic/BasicWinFormsEngineInitilizer/BasicWinFormsEngineInitilizer_ListBox.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+using System.Windows.Forms;
+
+namespace Crunchy.Sack.WinForms
+{
+    using Dough;
+    using Sack;
+    using WinCommon;
+    
+    [BasicWinFormsEngineInitilizer]
+    static public class BasicWinFormsEngineInitilizer_ListBox
+    {
+        [BasicWinFormsEngineInitilizer]
+        static public void Initilize(WinFormsEngine engine)
+        {
+            engine.AddPublicPropertyAttributeLinksForType<ListBox>();
+            engine.Add(
+                WinFormsInstancers.Simple("ListBox", () => new ListBox()),
+                WinFormsInfos.Children<ListBox>(b => b.Items)
+            );
+        }
+    }
+}
---
+++ CrunchySack_WinForms/Basic/BasicWinFormsEngineInitilizer/BasicWinFormsEngineInitilizer_Slider.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+using System.Windows.Forms;
+
+namespace Crunchy.Sack.WinForms
+{
+    using Dough;
+    using Sack;
+    using WinCommon;
+    
+    [BasicWinFormsEngineInitilizer]
+    static public class BasicWinFormsEngineInitilizer_Slider
+    {
+        [BasicWinFormsEngineInitilizer]
+        static public void Initilize(WinFormsEngine engine)
+        {
+            engine.AddPublicPropertyAttributeLinksForType<TrackBar>();
+            engine.Add(
+                WinFormsInstancers.Simple("Slider", () => new TrackBar())
+            );
+        }
+    }
+}
