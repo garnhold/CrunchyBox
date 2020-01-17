ccc5ef3e65c5a7fe75e87b696ab67f9f8ae96913
Garrett Arnhold <garrett@arnholdsystems.com>
2020-01-16 23:47:05 -0800

    Added Support for ItemsControl to CrunchySack_Avalonia

---
+++ CrunchySack_Avalonia/Basic/BasicAvaloniaEngineInitilizer/BasicAvaloniaEngineInitilizer_ItemsControl.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Interactivity;
+
+namespace Crunchy.Sack_Avalonia
+{
+    using Dough;
+    using Sack;
+    using Sack_WinCommon;
+    
+    [BasicAvaloniaEngineInitilizer]
+    static public class BasicAvaloniaEngineInitilizer_ItemsControl
+    {
+        [BasicAvaloniaEngineInitilizer]
+        static public void Initilize(AvaloniaEngine engine)
+        {
+            engine.AddPublicPropertyAttributeLinksForType<ItemsControl>();
+            engine.Add(
+                AvaloniaInfos.Children<ItemsControl, object>((c, os) => c.Items = os)
+            );
+        }
+    }
+}
