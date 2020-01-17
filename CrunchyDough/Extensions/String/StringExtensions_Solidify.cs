4c5847d5d6196f9482a9007e1a8e23c676ded4f4
Garrett Arnhold <garrett@arnholdsystems.com>
2020-01-16 23:28:42 -0800

    Added string.Solidify Extension Method

---
+++ CrunchyDough/Extensions/String/StringExtensions_Solidify.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Crunchy.Dough
+{
+    static public class StringExtensions_Solidify
+    {
+        static public string Solidify(this string input)
+        {
+            return input ?? "";
+        }
+    }
+}
