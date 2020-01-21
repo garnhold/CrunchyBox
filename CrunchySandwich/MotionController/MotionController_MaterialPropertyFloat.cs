e4b0aa9af4d75a02732ff928e8a0dc60417ff988
Garrett Arnhold <garrett@arnholdsystems.com>
2020-01-20 18:38:23 -0800

    Added MotionController_MaterialPropertyFloat

---
+++ CrunchySandwich/MotionController/MotionController_MaterialPropertyFloat.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+namespace Crunchy.Sandwich
+{
+    using Dough;
+    using Bun;
+    
+    [AddComponentMenu("Motion/MotionController_MaterialPropertyFloat")]
+    public class MotionController_MaterialPropertyFloat : MotionController
+    {
+        [SerializeField]private string material_property;
+        [SerializeFieldEX]private FloatRange range;
+
+        protected override void UpdateInternal(float value)
+        {
+            this.GetComponent<MeshRenderer>().AlterMaterialPropertyFloat(material_property, value.ConvertFromOffsetToRange(range));
+        }
+    }
+}
