a7c652c58de8e1e00d89e54aec35f7b30c66891a
Garrett Arnhold <garrett@arnholdsystems.com>
2020-01-26 16:25:43 -0800

    Tidied IList2D Solid Extensions + Added TraverseEdges

--- CrunchyDough/IList2D/Extensions/Special/IList2DExtensions_Solid.cs
+++ CrunchyDough/IList2D/Extensions/Special/IList2DExtensions_Solid.cs
@@ -8,10 +8,10 @@ namespace Crunchy.Dough
     {
         static public IList2D<bool> ConvertToEdges(this IList2D<bool> item)
         {
-            return item.ConvertWithIndexs(delegate (int x, int y, bool value) {
+            return item.ConvertWithIndexs(delegate (VectorI2 index, bool value) {
                 if (value)
                 {
-                    if (item.GetNeighbors(x, y).Has(false))
+                    if (index.GetNeighbors().Convert(p => item.Get(p)).Has(false))
                         return true;
                 }
 
@@ -23,15 +23,44 @@ namespace Crunchy.Dough
         {
             IList2D<bool> edges = item.ConvertToEdges().ToList2D();
 
-            return edges.ConvertWithIndexs(delegate(int x, int y, bool value) {
+            return edges.ConvertWithIndexs(delegate (VectorI2 index, bool value) {
                 if (value)
                 {
-                    if (edges.GetNeighbors(x, y).Count(false) >= 4)
+                    if (index.GetNeighbors().Convert(p => edges.Get(p)).Count(false) >= 4)
                         return true;
                 }
 
                 return false;
             });
+        }
+
+        static public IEnumerable<IEnumerable<VectorI2>> TraverseEdges(this IList2D<bool> item, int maximum_gap)
+        {
+            VectorI2 point;
+            IList2D<bool> edges = item.ConvertToStrictEdges().ToList2D();
+
+            for (int y = 0; y < edges.GetHeight(); y++)
+            {
+                for (int x = 0; x < edges.GetWidth(); x++)
+                {
+                    point = new VectorI2(x, y);
+
+                    if (edges.Get(point))
+                    {
+                        List<VectorI2> points = new List<VectorI2>();
+
+                        do
+                        {
+                            edges.SetDropped(point, false);
+                            points.Add(point);
+                        }
+                        while (point.GetRadiating(maximum_gap).TryFindFirst(p => edges.Get(p), out point));
+
+                        if (points.Count >= 3)
+                            yield return points;
+                    }
+                }
+            }
         }
     }
 }
