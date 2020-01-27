15b45d84837eee68923142461b6d471aedafb2ab
Garrett Arnhold <garrett@arnholdsystems.com>
2020-01-27 12:57:23 -0800

    Removed IList2DCell

--- CrunchyDough/IList2D/Extensions/IList2DExtensions_IEnumerable.cs
+++ CrunchyDough/IList2D/Extensions/IList2DExtensions_IEnumerable.cs
@@ -18,18 +18,6 @@ namespace Crunchy.Dough
             }
         }
 
-        static public IEnumerable<IList2DCell<T>> GetCells<T>(this IList2D<T> item)
-        {
-            int height = item.GetHeight();
-            int width = item.GetWidth();
-
-            for (int y = 0; y < height; y++)
-            {
-                for (int x = 0; x < width; x++)
-                    yield return new IList2DCell<T>(x, y, item);
-            }
-        }
-
         static public IEnumerable<KeyValuePair<VectorI2, T>> GetKeyValuePairs<T>(this IList2D<T> item)
         {
             int height = item.GetHeight();
