871787e5f15ebc41d38c02930f0c546ea0767d36
Garrett Arnhold <garrett@arnholdsystems.com>
2020-01-26 16:12:58 -0800

    Added VectorI2 Neighbor Iterators

---
+++ CrunchyDough/VectorI2/Extensions/VectorI2Extensions_Neighbors.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Crunchy.Dough
+{    
+    static public class VectorI2Extensions_Neighbors
+    {
+        static public IEnumerable<VectorI2> GetCardinalNeighbors(this VectorI2 item)
+        {
+            yield return new VectorI2(item.x, item.y - 1);
+            yield return new VectorI2(item.x - 1, item.y);
+            yield return new VectorI2(item.x + 1, item.y);
+            yield return new VectorI2(item.x, item.y + 1);
+        }
+
+        static public IEnumerable<VectorI2> GetOrdinalNeighbors(this VectorI2 item)
+        {
+            yield return new VectorI2(item.x - 1, item.y - 1);
+            yield return new VectorI2(item.x + 1, item.y - 1);
+            yield return new VectorI2(item.x - 1, item.y + 1);
+            yield return new VectorI2(item.x + 1, item.y + 1);
+        }
+
+        static public IEnumerable<VectorI2> GetCardinalThenOrdinalNeighbors(this VectorI2 item)
+        {
+            return item.GetCardinalNeighbors().Append(item.GetOrdinalNeighbors());
+        }
+        static public IEnumerable<VectorI2> GetNeighbors(this VectorI2 item)
+        {
+            return item.GetCardinalThenOrdinalNeighbors();
+        }
+    }
+}
---
+++ CrunchyDough/VectorI2/Extensions/VectorI2Extensions_Radiating.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Crunchy.Dough
+{    
+    static public class VectorI2Extensions_Radiating
+    {
+        static public IEnumerable<VectorI2> GetRadiating(this VectorI2 item, int radius)
+        {
+            return RadiatingWalker.Iterator(radius).Convert(d => item + d);
+        }
+    }
+}
