73b76b8368a463dbb892cd5509687abda236284f
Garrett Arnhold <garrett@arnholdsystems.com>
2020-01-28 01:27:23 -0800

    Removed ISparseGrid
    I think I'll instead be removing Crunchy.Sauce 's Surface 's point fetching stuff.

--- CrunchyDough/Empty/Empty.cs
+++ CrunchyDough/Empty/Empty.cs
@@ -21,11 +21,6 @@ namespace Crunchy.Dough
             return EmptyGrid<T>.INSTANCE;
         }
 
-        static public ISparseGrid<T> ISparseGrid<T>()
-        {
-            return EmptySparseGrid<T>.INSTANCE;
-        }
-
         static public ICollection<T> ICollection<T>()
         {
             return EmptyList<T>.INSTANCE;
