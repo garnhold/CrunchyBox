75cc4c41fc3958082eb52f33fa8a0f1a7463c913
Garrett Arnhold <garrett@arnholdsystems.com>
2020-01-29 16:08:09 -0800

    Added ListBox to the Avalonia Sack

---
+++ CrunchySack_Avalonia/Basic/BasicAvaloniaEngineInitilizer/BasicAvaloniaEngineInitilizer_ListBox.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Interactivity;
+
+namespace Crunchy.Sack_Avalonia
+{
+    using Dough;
+    using Sack;
+    using Sack_WinCommon;
+    
+    [BasicAvaloniaEngineInitilizer]
+    static public class BasicAvaloniaEngineInitilizer_ListBox
+    {
+        [BasicAvaloniaEngineInitilizer]
+        static public void Initilize(AvaloniaEngine engine)
+        {
+            engine.AddSimpleInstancer<ListBox>();
+            engine.AddAvaloniaPropertyAttributeLinksForType<ListBox>();
+            engine.AddChildren<ListBox, ListBoxItem>((b, o) => b.Items = o);
+
+            engine.AddSimpleInstancer<ListBoxItem>();
+            engine.AddAvaloniaPropertyAttributeLinksForType<ListBoxItem>();
+        }
+    }
+}
