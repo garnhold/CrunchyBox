858dc881b3c7df5e9bb8ff2572456fa4149241c2
Garrett Arnhold <garrett@arnholdsystems.com>
2020-01-31 00:26:48 -0800

    Added GizmosExtensions.DrawArearGrid

--- CrunchySandwich/Extensions/Gizmos/GizmosExtensions_Grid.cs
+++ CrunchySandwich/Extensions/Gizmos/GizmosExtensions_Grid.cs
@@ -20,5 +20,21 @@ namespace Crunchy.Sandwich
         {
             DrawPlanarGrid(rect.min, rect.max, cell_size);
         }
+
+        static public void DrawArearGrid(Vector2 minimum, Vector2 maximum, Vector2 cell_size, float y = 0.0f)
+        {
+            Vector3 minimum3 = minimum.GetArear(y);
+            Vector3 maximum3 = maximum.GetArear(y);
+
+            Floats.Range(minimum.y, maximum.y, cell_size.y, true)
+                .Process(z => Gizmos.DrawLine(minimum3.GetWithZ(z), maximum3.GetWithZ(z)));
+
+            Floats.Range(minimum.x, maximum.x, cell_size.x, true)
+                .Process(x => Gizmos.DrawLine(minimum3.GetWithX(x), maximum3.GetWithX(x)));
+        }
+        static public void DrawArearGrid(Rect rect, Vector2 cell_size, float y = 0.0f)
+        {
+            DrawArearGrid(rect.min, rect.max, cell_size, y);
+        }
     }
 }
