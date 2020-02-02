240ef6556fc9d16f54328e5940bfd449bbff7a73
Garrett Arnhold <garrett@arnholdsystems.com>
2020-02-02 15:24:53 -0800

    Added EffigyInfo.AddGetChild

---
+++ CrunchySack/Link/EffigyLink/EffigyInfo/Extensions/EffigyInfoExtensions.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Crunchy.Sack
+{
+    using Dough;
+    using Salt;
+    using Noodle;
+    
+    static public class EffigyInfoExtensions
+    {
+    }
+}
