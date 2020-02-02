cc664339103bb5700475ee90dfeebde8b67c9f0c
Garrett Arnhold <garrett@arnholdsystems.com>
2020-02-01 20:28:58 -0800

    Some Work on CrunchySack_Gtk

---
+++ CrunchySack_Gtk/Extensions/Container/ContainerExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+
+using Gtk;
+
+namespace Crunchy.Sack_Gtk
+{
+    using Dough;
+    using Noodle;
+    using Sack;
+    
+    static public class ContainerExtensions
+    {
+    }
+}
---
+++ CrunchySack_Gtk/Extensions/Container/ContainerExtensions_Children.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+
+using Gtk;
+
+namespace Crunchy.Sack_Gtk
+{
+    using Dough;
+    using Noodle;
+    using Sack;
+    
+    static public class ContainerExtensions_Children
+    {
+        static public void ClearChildren(this Container item)
+        {
+            item.Children.ProcessCopy(w => item.Remove(w));
+        }
+
+        static public void RemoveChildAt(this Container item, int index)
+        {
+            item.Remove(item.GetChildAt(index));
+        }
+
+        static public void AddChild(this Container item, Widget widget)
+        {
+            item.Add(widget);
+        }
+
+        static public void InsertChild(this Container item, int index, Widget widget)
+        {
+            Widget[] new_children = item.Children.CopyInsert(index, widget);
+            fasdadsffadsadsf
+        }
+
+
+
+        static public Widget GetChildAt(this Container item, int index)
+        {
+            return item.Children.Get(index);
+        }
+    }
+}
