274e076e6d71f1416d0be2a21d1d2c811a67255f
Garrett Arnhold <garrett@arnholdsystems.com>
2020-02-03 16:36:09 -0800

    Added Menu Stuff to the Gtk Sack

---
+++ CrunchySack_Gtk/Basic/BasicGtkEngineInitilizer/BasicGtkEngineInitilizer_Menu.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+using Gtk;
+
+namespace Crunchy.Sack_Gtk
+{
+    using Dough;
+    using Sack;
+    
+    [BasicGtkEngineInitilizer]
+    static public class BasicGtkEngineInitilizer_Menu
+    {
+        [BasicGtkEngineInitilizer]
+        static public void Initilize(GtkEngine engine)
+        {
+            engine.AddSimpleInstancer<MenuBar>("Menu");
+            engine.AddPublicPropertyAttributeLinksForType<MenuBar>();
+
+            engine.AddSimpleInstancer<MenuItem>("MenuItem");
+            engine.AddPublicPropertyAttributeLinksForType<MenuItem>();
+
+            engine.AddChildren<MenuItem, MenuItem>(
+                (i, p) => i.RemoveSubmenuChildAt(p),
+                (i, c) => i.AddSubmenuChild(c),
+                (i, p, c) => i.InsertSubmenuChild(p, c)
+            );
+        }
+    }
+}
---
+++ CrunchySack_Gtk/Extensions/MenuItem/MenuItemExtensions_Submenu.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+
+using Gtk;
+
+namespace Crunchy.Sack_Gtk
+{
+    using Dough;
+    using Noodle;
+    using Sack;
+    
+    static public class MenuItemExtensions_Submenu
+    {
+        static public Menu FetchSubmenu(this MenuItem item)
+        {
+            if (item.Submenu == null)
+                item.Submenu = new Menu();
+
+            return item.Submenu as Menu;
+        }
+
+        static public void ClearSubmenu(this MenuItem item)
+        {
+            item.Submenu = null;
+        }
+
+        static public void RemoveSubmenuChildAt(this MenuItem item, int index)
+        {
+            item.FetchSubmenu().RemoveChildAt(index);
+        }
+
+        static public void AddSubmenuChild(this MenuItem item, Widget widget)
+        {
+            item.FetchSubmenu().AddChild(widget);
+        }
+
+        static public void InsertSubmenuChild(this MenuItem item, int index, Widget widget)
+        {
+            item.FetchSubmenu().InsertChild(index, widget);
+        }
+    }
+}
