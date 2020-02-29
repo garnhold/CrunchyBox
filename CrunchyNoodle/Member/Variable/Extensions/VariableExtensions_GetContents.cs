6f8e2301a41c70ae3101e202e0a772e3b9eb3d4d
Garrett Arnhold <garrett@arnholdsystems.com>
2020-02-29 14:50:38 -0800

    Added Variable.GetOrCreateContents Extension Methods

--- CrunchyNoodle/Member/Variable/Extensions/VariableExtensions_GetContents.cs
+++ CrunchyNoodle/Member/Variable/Extensions/VariableExtensions_GetContents.cs
@@ -10,5 +10,22 @@ namespace Crunchy.Noodle
         {
             return item.GetContents(target).Convert<T>();
         }
+
+        static public object GetOrCreateContents(this Variable item, object target, Operation<object> operation)
+        {
+            object current_contents = item.GetContents(target);
+
+            if (current_contents == null)
+            {
+                current_contents = operation();
+                item.SetContents(target, current_contents);
+            }
+
+            return current_contents;
+        }
+        static public T GetOrCreateContents<T>(this Variable item, object target, Operation<T> operation)
+        {
+            return item.GetOrCreateContents(target, (Operation<object>)(() => operation())).Convert<T>();
+        }
     }
 }
