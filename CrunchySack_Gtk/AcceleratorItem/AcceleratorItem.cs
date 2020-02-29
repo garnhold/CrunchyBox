8c66214e02c2a4aacb23440e9fc1f81d74b08ab5
Garrett Arnhold <garrett@arnholdsystems.com>
2020-02-29 14:52:58 -0800

    Added Addtional Constructors to AcceleratorItem

--- CrunchySack_Gtk/AcceleratorItem/AcceleratorItem.cs
+++ CrunchySack_Gtk/AcceleratorItem/AcceleratorItem.cs
@@ -24,6 +24,11 @@ namespace Crunchy.Sack_Gtk
         public AcceleratorItem(string s, Gdk.Key k, Gdk.ModifierType m, AccelFlags f) : this(s, new AccelKey(k, m, f)) { }
         public AcceleratorItem(string s, Gdk.Key k, Gdk.ModifierType m) : this(s, k, m, AccelFlags.Visible) { }
 
+        public AcceleratorItem(string s, string p, AccelFlags f) : this(s, AccelKeyExtensions.Parse(p, f)) { }
+        public AcceleratorItem(string s, string p) : this(s, p, AccelFlags.Visible) { }
+
+        public AcceleratorItem(string p) : this("activate", p) { }
+
         public string GetSignal()
         {
             return signal;
