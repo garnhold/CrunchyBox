c1df190fe9825b1185ec608cf58c3f572bd63c81
Garrett Arnhold <garrett@arnholdsystems.com>
2020-02-29 14:56:30 -0800

    Added AccelKeyExtensions

---
+++ CrunchySack_Gtk/Extensions/AccelKey/AccelKeyExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+
+using Gtk;
+using Gdk;
+
+namespace Crunchy.Sack_Gtk
+{
+    using Dough;
+    using Noodle;
+    using Sack;
+    
+    static public class AccelKeyExtensions
+    {
+        static public AccelKey Parse(string input, AccelFlags flags)
+        {
+            uint key;
+            ModifierType modifier;
+
+            Accelerator.Parse(input, out key, out modifier);
+            return new AccelKey((Gdk.Key)key, modifier, flags);
+        }
+    }
+}
