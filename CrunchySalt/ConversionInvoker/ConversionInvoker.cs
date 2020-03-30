0e69618c6a35e3faf9ea22e856072c14ae13e8eb
Garrett Arnhold <garrett@arnholdsystems.com>
2020-03-30 00:01:24 -0700

    Added ConversionInvoker

---
+++ CrunchySalt/ConversionInvoker/ConversionInvoker.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Reflection;
+using System.Reflection.Emit;
+
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Crunchy.Salt
+{
+    using Dough;
+    
+    public delegate object BasicConversionInvoker(object value);
+}
