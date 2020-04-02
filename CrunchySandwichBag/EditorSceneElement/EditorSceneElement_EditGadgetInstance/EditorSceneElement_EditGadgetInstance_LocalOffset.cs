fbfa0fd0fc503d2d5067fc0a96a3f7e69f0964d5
Garrett Arnhold <garrett@arnholdsystems.com>
2020-04-01 22:08:16 -0700

    Added LocalOffset EditGadget

---
+++ CrunchySandwichBag/EditorSceneElement/EditorSceneElement_EditGadgetInstance/EditorSceneElement_EditGadgetInstance_LocalOffset.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Reflection;
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEditor;
+
+namespace Crunchy.SandwichBag
+{
+    using Dough;
+    using Sauce;
+    using Sandwich;
+    
+    public class EditorSceneElement_EditGadgetInstance_LocalOffset : EditorSceneElement_EditGadgetInstance
+    {
+        protected override void DrawInternal()
+        {
+            float local_offset = this.GetContents<float>();
+            Vector3 local_direction = this.GetAuxContents<Vector3>("local_direction");
+            Matrix4x4 local_to_world_matrix = this.GetAuxContents<Matrix4x4>("local_to_world_matrix");
+
+            Vector3 local_point = local_direction * local_offset;
+
+            Vector3 world_point = Handles.DoPositionHandle(local_to_world_matrix.MultiplyPoint(local_point), Quaternion.identity);
+            Matrix4x4 world_to_local_matrix = local_to_world_matrix.inverse;
+
+            this.SetContents(
+                world_to_local_matrix.MultiplyPoint(world_point).GetDot(local_direction)
+            );
+        }
+
+        public EditorSceneElement_EditGadgetInstance_LocalOffset(EditGadgetInstance g) : base(g)
+        {
+        }
+    }
+}
