f0d1e0d5c058553cc56596134ddd50b23fb436a7
Garrett Arnhold <garrett@arnholdsystems.com>
2020-04-08 15:13:12 -0700

    Added ConfigurableJoint.SetupAsLimitedBallJoint

--- CrunchySandwich/Extensions/ConfigurableJoint/ConfigurableJointExtensions_Setup.cs
+++ CrunchySandwich/Extensions/ConfigurableJoint/ConfigurableJointExtensions_Setup.cs
@@ -22,5 +22,26 @@ namespace Crunchy.Sandwich
 
             return item;
         }
+
+        static public ConfigurableJoint SetupAsLimitedBallJoint(this ConfigurableJoint item, float free_angle, float spring = 0.0f, float damper = 0.0f)
+        {
+            item.xMotion = ConfigurableJointMotion.Locked;
+            item.yMotion = ConfigurableJointMotion.Locked;
+            item.zMotion = ConfigurableJointMotion.Locked;
+
+            item.angularXMotion = ConfigurableJointMotion.Limited;
+            item.angularYMotion = ConfigurableJointMotion.Free;
+            item.angularZMotion = ConfigurableJointMotion.Limited;
+
+            item.lowAngularXLimit = new SoftJointLimit() { limit = free_angle * -0.5f };
+            item.highAngularXLimit = new SoftJointLimit() { limit = free_angle * 0.5f };
+
+            item.angularXLimitSpring = new SoftJointLimitSpring() {
+                spring = spring,
+                damper = damper
+            };
+
+            return item;
+        }
     }
 }
