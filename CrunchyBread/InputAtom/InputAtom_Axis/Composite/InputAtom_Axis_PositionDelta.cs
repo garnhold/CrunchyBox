843b7fc47257229861aa95dd82596f596ac93133
Garrett Arnhold <garrett@arnholdsystems.com>
2020-04-08 22:12:32 -0700

    Added InputAtom_Axis_PositionDelta

---
+++ CrunchyBread/InputAtom/InputAtom_Axis/Composite/InputAtom_Axis_PositionDelta.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Crunchy.Bread
+{
+    using Dough;
+
+    public class InputAtom_Axis_PositionDelta : InputUpdateAtom, InputAtom_Axis
+    {
+        private InputAtom_Position position;
+
+        private FloatDelta delta;
+
+        protected override void StartAtomInternal()
+        {
+            delta.Clear();
+        }
+
+        protected override void UpdateAtomInternal()
+        {
+            delta.UpdateDelta(position.GetValue());
+        }
+
+        public InputAtom_Axis_PositionDelta(InputAtom_Position p)
+        {
+            position = p;
+
+            delta = new FloatDelta();
+        }
+
+        public override void EnterLockSection(InputAtomLock @lock)
+        {
+            position.EnterLockSection(@lock);
+        }
+
+        public override void ExitLockSection(InputAtomLock @lock)
+        {
+            position.ExitLockSection(@lock);
+        }
+
+        public override bool IsNominallyLocked()
+        {
+            return position.IsNominallyLocked();
+        }
+
+        public override bool IsEffectivelyLocked()
+        {
+            return position.IsEffectivelyLocked();
+        }
+
+        public float GetValue()
+        {
+            return delta.GetDelta();
+        }
+    }
+}
---
+++ CrunchyBread/InputAtom/InputUpdateAtom.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Crunchy.Bread
+{
+    using Dough;
+
+    public abstract class InputUpdateAtom : InputAtom
+    {
+        private Frame frame;
+
+        protected virtual void StartAtomInternal() { }
+        protected abstract void UpdateAtomInternal();
+
+        public abstract bool IsNominallyLocked();
+        public abstract bool IsEffectivelyLocked();
+
+        public abstract void EnterLockSection(InputAtomLock @lock);
+        public abstract void ExitLockSection(InputAtomLock @lock);
+
+        protected void Touch()
+        {
+            if (frame.IsNotCurrent())
+            {
+                if (frame.IsNotRecent())
+                    StartAtomInternal();
+
+                UpdateAtomInternal();
+                frame = Frame.GetCurrentFrame();
+            }
+        }
+    }
+}
