9bf8ef044fbb7f5277eefdcf1867782dbdbf4235
Garrett Arnhold <garrett@arnholdsystems.com>
2020-04-09 18:19:33 -0700

    Added <T> Convience Flavors to a Few TyonSetting Extensions

--- CrunchyRecipe/Tyon/Settings/Extensions/TyonSettingsExtensions_Convience.cs
+++ CrunchyRecipe/Tyon/Settings/Extensions/TyonSettingsExtensions_Convience.cs
@@ -24,9 +24,22 @@ namespace Crunchy.Recipe
         {
             return item.CreateContext().Deserialize(text, mode);
         }
+        static public T Deserialize<T>(this TyonSettings item, string text, TyonHydrationMode mode)
+        {
+            return item.Deserialize(text, mode).Convert<T>();
+        }
+
         static public object DeserializeValue(this TyonSettings item, Type type, string text, TyonHydrationMode mode)
         {
             return item.CreateContext().DeserializeValue(type, text, mode);
+        }
+        static public T DeserializeValue<T>(this TyonSettings item, Type type, string text, TyonHydrationMode mode)
+        {
+            return item.DeserializeValue(type, text, mode).Convert<T>();
+        }
+        static public T DeserializeValue<T>(this TyonSettings item, string text, TyonHydrationMode mode)
+        {
+            return item.DeserializeValue<T>(typeof(T), text, mode);
         }
 
         static public void DeserializeInto(this TyonSettings item, object obj, string text, TyonHydrationMode mode)
