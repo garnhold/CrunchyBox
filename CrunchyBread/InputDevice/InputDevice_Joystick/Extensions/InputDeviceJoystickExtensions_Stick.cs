307e639ec2dda0288b8c6404d93d2f40b60a37f1
Garrett Arnhold <garrett@arnholdsystems.com>
2020-04-09 18:16:41 -0700

    Added InputDevice_Joystick.GetAxisPairStick

---
+++ CrunchyBread/InputDevice/InputDevice_Joystick/Extensions/InputDeviceJoystickExtensions_Stick.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using OpenTK.Input;
+
+namespace Crunchy.Bread
+{
+    using Dough;
+
+    static public class InputDeviceJoystickExtensions_Stick
+    {
+        static public InputAtom_Stick GetAxisPairStick(this InputDevice_Joystick item, int horizontal_axis_index, int vertical_axis_index)
+        {
+            return new InputAtom_Stick_AxisPair(
+                item.GetAxis(horizontal_axis_index),
+                item.GetAxis(vertical_axis_index)
+            );
+        }
+    }
+}
