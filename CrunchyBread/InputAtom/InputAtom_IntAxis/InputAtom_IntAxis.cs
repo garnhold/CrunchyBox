58bca7ec44765f70010f82e4f7cd24de6081bf3b
Garrett Arnhold <garrett@arnholdsystems.com>
2020-04-11 14:00:30 -0700

    Added InputAtom_IntAxis

---
+++ CrunchyBread/InputAtom/InputAtom_IntAxis/InputAtom_IntAxis.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Crunchy.Bread
+{
+    using Dough;
+
+    public interface InputAtom_IntAxis : InputAtom<int>
+    {
+    }
+}
