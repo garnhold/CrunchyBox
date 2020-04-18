8770ebe55f857440863f8c009399e4e0186da547
Garrett Arnhold <garrett@arnholdsystems.com>
2020-04-17 21:07:31 -0700

    Added InputAtom_Axis_Multiple

---
+++ CrunchyBread/InputAtom/InputAtom_Axis/Composite/InputAtom_Axis_Multiple.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Crunchy.Bread
+{
+    using Dough;
+
+    public class InputAtom_Axis_Multiple : InputCompositeAtom, InputAtom_Axis
+    {
+        private List<InputAtom_Axis> axises;
+
+        protected override IEnumerable<InputAtom> GetAtoms()
+        {
+            return axises.Convert<InputAtom_Axis, InputAtom>();
+        }
+
+        public InputAtom_Axis_Multiple(IEnumerable<InputAtom_Axis> a)
+        {
+            axises = a.ToList();
+        }
+
+        public InputAtom_Axis_Multiple(params InputAtom_Axis[] a) : this((IEnumerable<InputAtom_Axis>)a) { }
+
+        public float GetRawValue()
+        {
+            return axises.Convert(a => a.GetRawValue()).Sum().BindOffset();
+        }
+    }
+}
