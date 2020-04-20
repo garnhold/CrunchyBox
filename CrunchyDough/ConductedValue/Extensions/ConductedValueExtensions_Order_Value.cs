448e62a0cd070badf815f8081516c913bf643e5a
Garrett Arnhold <garrett@arnholdsystems.com>
2020-04-19 17:28:29 -0700

    Simplified some ConductedValue Extensions

--- CrunchyDough/ConductedValue/Extensions/ConductedValueExtensions_Order_Value.cs
+++ CrunchyDough/ConductedValue/Extensions/ConductedValueExtensions_Order_Value.cs
@@ -17,9 +17,10 @@ namespace Crunchy.Dough
 
         static private IEnumerable<ConductorOrder> InternalOrder_FlickerValue<T>(ConductedValue<T> item, T first, T second)
         {
-            yield return item.Order_SetValue(first);
+            item.SetValue(first);
             yield return item.Order_WaitForNextUpdate();
-            yield return item.Order_SetValue(second);
+            item.SetValue(second);
+
         }
         static public ConductorOrder Order_FlickerValue<T>(this ConductedValue<T> item, T first, T second)
         {
@@ -30,9 +31,9 @@ namespace Crunchy.Dough
         {
             T old_value = item.GetValue();
 
-            yield return item.Order_SetValue(value);
+            item.SetValue(value);
             yield return item.Order_WaitForNextUpdate();
-            yield return item.Order_SetValue(old_value);
+            item.SetValue(old_value);
         }
         static public ConductorOrder Order_FlickerValue<T>(this ConductedValue<T> item, T value)
         {
