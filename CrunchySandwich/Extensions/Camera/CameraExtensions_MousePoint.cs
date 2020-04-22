386407a40dd4c3e0d50aa1bbc51cc83eb4f64c0f
Garrett Arnhold <garrett@arnholdsystems.com>
2020-04-21 17:43:05 -0700

    Added Camera.MouseToWorldRay Convience Method

---
+++ CrunchySandwich/Extensions/Camera/CameraExtensions_MousePoint.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+namespace Crunchy.Sandwich
+{
+    using Dough;
+
+    static public class CameraExtensions_MousePoint
+    {
+        static public Ray MouseToWorldRay(this Camera item)
+        {
+            return item.ScreenPointToRay(Event.current.mousePosition.ConvertFromGUIToScreen());
+        }
+    }
+}
