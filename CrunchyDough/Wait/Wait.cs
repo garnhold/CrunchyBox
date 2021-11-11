9aff78d3706f6d2ecca5989b24b00ae4b663ada3
Garrett Arnhold <garrett@arnholdsystems.com>
2021-11-10 23:11:02 -0800

    Added Missing 'public' Modifier to Wait Method

--- CrunchyDough/Wait/Wait.cs
+++ CrunchyDough/Wait/Wait.cs
@@ -7,20 +7,20 @@ namespace Crunchy.Dough
 {
     static public class Wait
     {
-        static async Task ForDuration(Duration duration)
+        static public async Task ForDuration(Duration duration)
         {
             await Task.Delay((int)duration.GetWholeMilliseconds());
         }
-        static async Task ForMilliseconds(long milliseconds)
+        static public async Task ForMilliseconds(long milliseconds)
         {
             await Wait.ForDuration(Duration.Milliseconds(milliseconds));
         }
-        static async Task ForSeconds(float seconds)
+        static public async Task ForSeconds(float seconds)
         {
             await Wait.ForDuration(Duration.Seconds(seconds));
         }
 
-        static async Task Until(Predicate predicate, Duration check_interval)
+        static public async Task Until(Predicate predicate, Duration check_interval)
         {
             while (predicate() == false)
                 await Wait.ForDuration(check_interval);
