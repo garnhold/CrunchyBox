722165d693a7ab7e2f8983c2e8b8f8126deb294d
Garrett Arnhold <garrett@arnholdsystems.com>
2021-11-10 23:09:27 -0800

    Simplified Wait

--- CrunchyDough/Wait/Wait.cs
+++ CrunchyDough/Wait/Wait.cs
@@ -7,25 +7,17 @@ namespace Crunchy.Dough
 {
     static public class Wait
     {
-        static Task ForDuration(Duration duration)
+        static async Task ForDuration(Duration duration)
         {
-            System.Timers.Timer timer = new System.Timers.Timer();
-            TaskCompletionSource<bool> task_completion_source = new TaskCompletionSource<bool>();
-
-            timer.Elapsed += (sender, e) => task_completion_source.TrySetResult(true);
-            timer.Interval = duration.GetMilliseconds();
-            timer.AutoReset = false;
-            timer.Start();
-
-            return task_completion_source.Task;
+            await Task.Delay((int)duration.GetWholeMilliseconds());
         }
-        static Task ForMilliseconds(long milliseconds)
+        static async Task ForMilliseconds(long milliseconds)
         {
-            return Wait.ForDuration(Duration.Milliseconds(milliseconds));
+            await Wait.ForDuration(Duration.Milliseconds(milliseconds));
         }
-        static Task ForSeconds(float seconds)
+        static async Task ForSeconds(float seconds)
         {
-            return Wait.ForDuration(Duration.Seconds(seconds));
+            await Wait.ForDuration(Duration.Seconds(seconds));
         }
 
         static async Task Until(Predicate predicate, Duration check_interval)
