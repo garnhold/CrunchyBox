6c118a3596f8dbc446dc9f2c405d81ac2304b6d0
Garrett Arnhold <garrett@arnholdsystems.com>
2021-11-11 20:04:38 -0800

    Added Choreography

---
+++ CrunchyDough/Choreography/Choreography.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Threading.Tasks;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Crunchy.Dough
+{
+    public class Choreography
+    {
+        private List<TaskCompletionSource<bool>> incoming_sources;
+        private List<TaskCompletionSource<bool>> active_sources;
+
+        public Choreography()
+        {
+            incoming_sources = new List<TaskCompletionSource<bool>>();
+            active_sources = new List<TaskCompletionSource<bool>>();
+        }
+
+        public void Release()
+        {
+            incoming_sources.Process(s => s.SetCanceled());
+            active_sources.Process(s => s.SetCanceled());
+        }
+
+        public void Update()
+        {
+            active_sources.Process(s => s.SetResult(true));
+
+            Swap.Values(ref active_sources, ref incoming_sources);
+            incoming_sources.Clear();
+        }
+
+        public Task ForUpdate()
+        {
+            TaskCompletionSource<bool> source = new TaskCompletionSource<bool>();
+
+            incoming_sources.Add(source);
+            return source.Task;
+        }
+
+        public async Task ForCondition(Predicate predicate)
+        {
+            while (predicate() == false)
+                await ForUpdate();
+        }
+
+        public async Task ForDuration(Duration duration)
+        {
+            Timer timer = new Timer(duration).StartAndGet();
+
+            await ForCondition(() => timer.IsTimeOver());
+        }
+        public async Task ForMilliseconds(long milliseconds)
+        {
+            await ForDuration(Duration.Milliseconds(milliseconds));
+        }
+        public async Task ForSeconds(float seconds)
+        {
+            await ForDuration(Duration.Seconds(seconds));
+        }
+    }
+}
