c3d3a11aa46e7ca4d6af314c383f1c94335a1dae
Garrett Arnhold <garrett@arnholdsystems.com>
2021-11-16 17:16:03 -0800

    Added MethodInfo.IsProp/Settish/Gettish/ish

--- CrunchySalt/Extensions/MethodInfo/MethodInfoExtensions_Compare.cs
+++ CrunchySalt/Extensions/MethodInfo/MethodInfoExtensions_Compare.cs
@@ -13,6 +13,29 @@ namespace Crunchy.Salt
     
     static public class MethodInfoExtensions_Compare
     {
+        static public bool IsPropSettish(this MethodInfo item)
+        {
+            if (item.IsPropSetCompatible() && item.Name.StartsWithAny("set", "Set"))
+                return true;
+
+            return false;
+        }
+        static public bool IsPropGettish(this MethodInfo item)
+        {
+            if (item.IsPropGetCompatible() && item.Name.StartsWithAny("get", "Get"))
+                return true;
+
+            return false;
+        }
+
+        static public bool IsPropish(this MethodInfo item)
+        {
+            if (item.IsPropSettish() || item.IsPropGettish())
+                return true;
+
+            return false;
+        }
+
         static public bool IsPropSetCompatible(this MethodInfo item)
         {
             if (item.HasNoReturn() && item.GetNumberEffectiveParameters() == 1)
