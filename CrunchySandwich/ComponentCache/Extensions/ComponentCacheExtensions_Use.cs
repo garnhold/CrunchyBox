a0641ecaed73c1c6c50b4ecc90b320a4bb7552a3
Garrett Arnhold <garrett@arnholdsystems.com>
2021-11-16 22:37:41 -0800

    Changed ComponentCache.IfNotNull -> Use
    Did this so there wasn't two versions of IfNotNull.

---
+++ CrunchySandwich/ComponentCache/Extensions/ComponentCacheExtensions_Use.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+namespace Crunchy.Sandwich
+{
+    using Dough;
+
+    static public class ComponentCacheExtensions_Use
+    {
+        static public void Use<T>(this ComponentCache<T> item, Process<T> process)
+        {
+            item.IfNotNull(i => i.GetComponent().IfNotNull(process));
+        }
+        static public void Use<T>(this ComponentCache<T> item, Process<T> process, Process if_null)
+        {
+            item.IfNotNull(i => i.GetComponent().IfNotNull(process, if_null), if_null);
+        }
+
+        static public J Use<J, T>(this ComponentCache<T> item, Operation<J, T> operation)
+        {
+            return item.IfNotNull(i => i.GetComponent().IfNotNull(operation));
+        }
+        static public J Use<J, T>(this ComponentCache<T> item, Operation<J, T> operation, Operation<J> if_null)
+        {
+            return item.IfNotNull(i => i.GetComponent().IfNotNull(operation, if_null), if_null);
+        }
+        static public J Use<J, T>(this ComponentCache<T> item, Operation<J, T> operation, J if_null)
+        {
+            return item.IfNotNull(i => i.GetComponent().IfNotNull(operation, if_null), if_null);
+        }
+    }
+}
