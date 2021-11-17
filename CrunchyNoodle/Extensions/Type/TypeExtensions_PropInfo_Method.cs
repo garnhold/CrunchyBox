0f2ebde45e0364ac633a8a28e5d9446e2c3b5f37
Garrett Arnhold <garrett@arnholdsystems.com>
2021-11-16 19:26:52 -0800

    Fixed TypeExtensions_PropInfo_Method

--- CrunchyNoodle/Extensions/Type/TypeExtensions_PropInfo_Method.cs
+++ CrunchyNoodle/Extensions/Type/TypeExtensions_PropInfo_Method.cs
@@ -9,31 +9,55 @@ namespace Crunchy.Noodle
     
     static public class TypeExtensions_PropInfo_Method
     {
-        static private OperationCache<IDictionary<string, PropInfoEX_MethodPair>, Type> GET_INSTANCE_METHOD_PROP_DICTIONARY = ReflectionCache.Get().NewOperationCache("GET_INSTANCE_METHOD_PROP_DICTIONARY", delegate (Type type) {
-            Pedia<string, PropInfoEX_MethodPair> methods = new Pedia<string, PropInfoEX_MethodPair>(() => new PropInfoEX_MethodPair());
-
-            foreach (MethodInfoEX method in type.GetFilteredInstanceMethods(Filterer_MethodInfo.IsPropCompatible()))
-            {
-                string name = method.Name.DetectEntityPropName();
-
-                if (method.IsPropSetCompatible())
-                    methods.GetValue(name).SetSetMethod(method);
-                else if (method.IsPropGetCompatible())
-                    methods.GetValue(name).SetGetMethod(method);
-            }
-
-            return (IDictionary<string, PropInfoEX_MethodPair>)methods.ToDictionary();
+        static private OperationCache<Dictionary<string, MethodInfoEX>, Type> GET_INSTANCE_GET_METHOD_DICTIONARY = ReflectionCache.Get().NewOperationCache("GET_INSTANCE_GET_METHOD_DICTIONARY", delegate (Type item) {
+            return item.GetFilteredInstanceMethods(Filterer_MethodInfo.IsPropGetCompatible())
+                .ToDictionarySkipValues(m => m.Name);
+        });
+        static private OperationCache<Dictionary<string, MethodInfoEX>, Type> GET_INSTANCE_SET_METHOD_DICTIONARY = ReflectionCache.Get().NewOperationCache("GET_INSTANCE_SET_METHOD_DICTIONARY", delegate (Type item) {
+            return item.GetFilteredInstanceMethods(Filterer_MethodInfo.IsPropSetCompatible())
+                .ToDictionarySkipValues(m => m.Name);
         });
 
-        static public PropInfoEX GetInstanceMethodProp(this Type item, string name)
+        static private OperationCache<PropInfoEX, Type, string, string> GET_INSTANCE_METHOD_PROP_INTERNAL = ReflectionCache.Get().NewOperationCache("GET_INSTANCE_METHOD_PROP_INTERNAL", delegate (Type item, string set_method, string get_method) {
+            MethodInfoEX set = GET_INSTANCE_SET_METHOD_DICTIONARY.Fetch(item).GetValue(set_method);
+            MethodInfoEX get = GET_INSTANCE_GET_METHOD_DICTIONARY.Fetch(item).GetValue(get_method);
+
+            if (set != null || get != null)
+                return (PropInfoEX)new PropInfoEX_MethodPair(set, get);
+
+            return null;
+        });
+        static public PropInfoEX GetInstanceMethodPropInternal(this Type item, string set_method, string get_method)
         {
-            return GET_INSTANCE_METHOD_PROP_DICTIONARY.Fetch(item)
-                .GetValue(name);
+            return GET_INSTANCE_METHOD_PROP_INTERNAL.Fetch(item, set_method, get_method);
         }
 
+        static private OperationCache<PropInfoEX, Type, string> GET_INSTANCE_METHOD_PROP = ReflectionCache.Get().NewOperationCache("GET_INSTANCE_METHOD_PROP", delegate (Type item, string name) {
+            string set_method;
+            string get_method;
+
+            name = name.TrimSuffix("()");
+            if (name.TryDetectEntityPropMethodPair(out set_method, out get_method))
+                return item.GetInstanceMethodPropInternal(set_method, get_method);
+
+            return null;
+        });
+        static public PropInfoEX GetInstanceMethodProp(this Type item, string name)
+        {
+            return GET_INSTANCE_METHOD_PROP.Fetch(item, name);
+        }
+
+        static private OperationCache<List<PropInfoEX>, Type> GET_ALL_INSTANCE_METHOD_PROPS = ReflectionCache.Get().NewOperationCache("GET_ALL_INSTANCE_METHOD_PROPS", delegate (Type type) {
+            return type.GetFilteredInstanceMethods(
+                Filterer_MethodInfo.IsPropCompatible()
+            ).Convert(m => type.GetInstanceMethodProp(m.Name))
+            .SkipNull()
+            .Unique()
+            .ToList();
+        });
         static public IEnumerable<PropInfoEX> GetAllInstanceMethodProps(this Type item)
         {
-            return GET_INSTANCE_METHOD_PROP_DICTIONARY.Fetch(item).Values;
+            return GET_ALL_INSTANCE_METHOD_PROPS.Fetch(item);
         }
     }
 }
