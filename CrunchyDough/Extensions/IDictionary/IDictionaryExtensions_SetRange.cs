9ef5b11aa6e2792fde2679005b97d718c7909adb
Garrett Arnhold <garrett@arnholdsystems.com>
2021-11-16 19:09:11 -0800

    Renamed IDictionary.SetRange -> SetRangeOverwrite

--- CrunchyDough/Extensions/IDictionary/IDictionaryExtensions_SetRange.cs
+++ CrunchyDough/Extensions/IDictionary/IDictionaryExtensions_SetRange.cs
@@ -6,13 +6,13 @@ namespace Crunchy.Dough
 {
     static public class IDictionaryExtensions_SetRange
     {
-        static public void SetRange<KEY_TYPE, ELEMENT_TYPE>(this IDictionary<KEY_TYPE, ELEMENT_TYPE> item, IEnumerable<KeyValuePair<KEY_TYPE, ELEMENT_TYPE>> to_set)
+        static public void SetRangeOverwrite<KEY_TYPE, ELEMENT_TYPE>(this IDictionary<KEY_TYPE, ELEMENT_TYPE> item, IEnumerable<KeyValuePair<KEY_TYPE, ELEMENT_TYPE>> to_set)
         {
             to_set.Process(p => item[p.Key] = p.Value);
         }
-        static public void SetRange<KEY_TYPE, ELEMENT_TYPE>(this IDictionary<KEY_TYPE, ELEMENT_TYPE> item, params KeyValuePair<KEY_TYPE, ELEMENT_TYPE>[] to_set)
+        static public void SetRangeOverwrite<KEY_TYPE, ELEMENT_TYPE>(this IDictionary<KEY_TYPE, ELEMENT_TYPE> item, params KeyValuePair<KEY_TYPE, ELEMENT_TYPE>[] to_set)
         {
-            item.SetRange((IEnumerable<KeyValuePair<KEY_TYPE, ELEMENT_TYPE>>)to_set);
+            item.SetRangeOverwrite((IEnumerable<KeyValuePair<KEY_TYPE, ELEMENT_TYPE>>)to_set);
         }
     }
 }
