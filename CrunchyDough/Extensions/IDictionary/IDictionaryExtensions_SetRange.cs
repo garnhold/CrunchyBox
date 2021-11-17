0323ca01d1133c012283d04a63e6bf642753ba89
Garrett Arnhold <garrett@arnholdsystems.com>
2021-11-16 19:10:16 -0800

    Added Dictionary.SetRangeSkip

--- CrunchyDough/Extensions/IDictionary/IDictionaryExtensions_SetRange.cs
+++ CrunchyDough/Extensions/IDictionary/IDictionaryExtensions_SetRange.cs
@@ -14,5 +14,17 @@ namespace Crunchy.Dough
         {
             item.SetRangeOverwrite((IEnumerable<KeyValuePair<KEY_TYPE, ELEMENT_TYPE>>)to_set);
         }
+
+        static public void SetRangeSkip<KEY_TYPE, ELEMENT_TYPE>(this IDictionary<KEY_TYPE, ELEMENT_TYPE> item, IEnumerable<KeyValuePair<KEY_TYPE, ELEMENT_TYPE>> to_set)
+        {
+            to_set.Process(p => {
+                if (item.ContainsKey(p.Key) == false)
+                    item[p.Key] = p.Value;
+            });
+        }
+        static public void SetRangeSkip<KEY_TYPE, ELEMENT_TYPE>(this IDictionary<KEY_TYPE, ELEMENT_TYPE> item, params KeyValuePair<KEY_TYPE, ELEMENT_TYPE>[] to_set)
+        {
+            item.SetRangeSkip((IEnumerable<KeyValuePair<KEY_TYPE, ELEMENT_TYPE>>)to_set);
+        }
     }
 }
