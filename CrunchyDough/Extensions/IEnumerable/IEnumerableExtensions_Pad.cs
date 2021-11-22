b19c711a565df2314b1a8ac819ddd8b4dee8bf8e
Garrett Arnhold <garrett@arnholdsystems.com>
2021-11-21 19:25:44 -0800

    Added IEnumerable.PadTo

---
+++ CrunchyDough/Extensions/IEnumerable/IEnumerableExtensions_Pad.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Crunchy.Dough
+{
+    static public class IEnumerableExtensions_Pad
+    {
+        static public IEnumerable<T> PadTo<T>(this IEnumerable<T> item, int target_length, Operation<T, int> operation)
+        {
+            int current_length = 0;
+
+            foreach (T sub_item in item)
+            {
+                yield return sub_item;
+                current_length++;
+            }
+
+            while (current_length < target_length)
+            {
+                yield return operation(current_length);
+                current_length++;
+            }
+        }
+        static public IEnumerable<T> PadTo<T>(this IEnumerable<T> item, int target_length, Operation<T> operation)
+        {
+            return item.PadTo(target_length, i => operation());
+        }
+    }
+}
