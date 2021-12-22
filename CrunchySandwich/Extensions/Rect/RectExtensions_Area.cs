fd2972f5d09d3952a147414cc5b84fb455cdafe3
Garrett Arnhold <garrett@arnholdsystems.com>
2021-12-22 13:47:43 -0800

    Added Rect.GetArea

---
+++ CrunchySandwich/Extensions/Rect/RectExtensions_Area.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+namespace Crunchy.Sandwich
+{
+    using Dough;
+
+    static public class RectExtensions_Area
+    {
+        static public float GetArea(this Rect item)
+        {
+            return item.width * item.height;
+        }
+    }
+}
