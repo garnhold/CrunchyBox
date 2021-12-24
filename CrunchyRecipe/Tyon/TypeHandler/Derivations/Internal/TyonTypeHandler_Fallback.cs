5267ce9928cdde0acdd5aa4dcebdcb6415d71043
Garrett Arnhold <garrett@arnholdsystems.com>
2021-12-24 12:20:15 -0800

    Tweaked TyonTypeHandler_Fallback to Be a Little Smarter About Surrogate Use

--- CrunchyRecipe/Tyon/TypeHandler/Derivations/Internal/TyonTypeHandler_Fallback.cs
+++ CrunchyRecipe/Tyon/TypeHandler/Derivations/Internal/TyonTypeHandler_Fallback.cs
@@ -17,10 +17,15 @@ namespace Crunchy.Recipe
 
         public override TyonValue Dehydrate(Type field_type, object value, TyonDehydrater dehydrater)
         {
-            if (field_type.CanHaveChildTypes())
-                return new TyonValue_Surrogate(value, dehydrater);
+            if (value.IsToStringOverridden())
+            {
+                if (field_type.CanHaveChildTypes())
+                    return new TyonValue_Surrogate(value, dehydrater);
 
-            return new TyonValue_String(value, dehydrater);
+                return new TyonValue_String(value, dehydrater);
+            }
+
+            return new TyonValue_Object(value, dehydrater);
         }
 
         public override bool IsCompatible(Type type)
