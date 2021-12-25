8a2ab43a759d3a14cff8693d9a1f9a778c2c5dfe
Garrett Arnhold <garrett@arnholdsystems.com>
2021-12-24 21:23:51 -0800

    Updated Scratch Program

--- Scratch/Program.cs
+++ Scratch/Program.cs
@@ -1,12 +1,44 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using Crunchy.Dough;
+using Crunchy.Recipe;
 
 namespace Scratch
 {
+    public class Poop
+    {
+        [TyonField] public RouletteWheel<string> wheel;
+        [TyonField] public List<KeyValuePair<string, float>> stuff;
+
+        public Poop()
+        {
+        }
+    }
+
     class MainClass
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Poop p = new Poop();
+
+            p.wheel = new RouletteWheel<string>(
+                KeyValuePair.New("hello", 2.0f),
+                KeyValuePair.New("hello hahaha", 1.0f)
+            );
+
+            p.wheel.GetRoulette().Deconstruct().Process(r => Console.WriteLine(r.Key));
+
+            string tyon = TyonSettings_General.INSTANCE.Serialize(p);
+
+            Console.WriteLine(tyon);
+
+            Poop deserialized = (Poop)TyonSettings_General.INSTANCE.Deserialize(tyon, TyonHydrationMode.Strict);
+
+            Console.WriteLine(TyonSettings_General.INSTANCE.Serialize(deserialized));
+            Console.ReadLine();
         }
+
     }
 }
