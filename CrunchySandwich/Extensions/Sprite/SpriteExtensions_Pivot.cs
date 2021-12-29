b4e67f2b3ff1666a810c2773bc89035e8eb39977
Garrett Arnhold <garrett@arnholdsystems.com>
2021-12-29 15:29:39 -0800

    Added SpriteExtensions_Pivo

---
+++ CrunchySandwich/Extensions/Sprite/SpriteExtensions_Pivot.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+namespace Crunchy.Sandwich
+{
+    using Dough;
+
+    static public class SpriteExtensions_Pivot
+    {
+        static public Vector2 GetPixelPivot(this Sprite item)
+        {
+            return item.pivot;
+        }
+
+        static public Vector2 GetNormalizedPivot(this Sprite item)
+        {
+            return item.GetPixelPivot().GetComponentDivide(item.GetTextureSize());
+        }
+
+        static public Vector2 GetWorldPivot(this Sprite item)
+        {
+            return item.GetNormalizedPivot().GetComponentMultiply(item.GetWorldSize());
+        }
+    }
+}
