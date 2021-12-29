f12d1627a1b8c9dafdf08910e8a330b59ea65f93
Garrett Arnhold <garrett@arnholdsystems.com>
2021-12-29 15:39:09 -0800

    Added Sprite.TransformNormalizedPoint

---
+++ CrunchySandwich/Extensions/Sprite/SpriteExtensions_Transform.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+namespace Crunchy.Sandwich
+{
+    using Dough;
+
+    static public class SpriteExtensions_Transform
+    {
+        static public Vector2 TransformNormalizedPoint(this Sprite item, Vector2 point)
+        {
+            return (point - item.GetNormalizedPivot()) * item.GetWorldSize();
+        }
+    }
+}
