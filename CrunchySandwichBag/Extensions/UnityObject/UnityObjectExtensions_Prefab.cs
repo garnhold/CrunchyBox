a743ee409b267d69a26267c34da864c82b268ac9
Garrett Arnhold <garrett@arnholdsystems.com>
2022-01-01 23:26:35 -0800

    Updated UnityObjectExtensions_Prefab to Use New API

--- CrunchySandwichBag/Extensions/UnityObject/UnityObjectExtensions_Prefab.cs
+++ CrunchySandwichBag/Extensions/UnityObject/UnityObjectExtensions_Prefab.cs
@@ -7,53 +7,24 @@ using UnityEditor;
 
 namespace Crunchy.SandwichBag
 {
-    using Dough;    
+    using Dough;
+
     static public class UnityObjectExtensions_Prefab
     {
         static public bool IsPrefab(this UnityEngine.Object item)
         {
-            PrefabType type = PrefabUtility.GetPrefabType(item);
+            if (PrefabUtility.GetPrefabAssetType(item) != PrefabAssetType.NotAPrefab)
+                return true;
 
-            switch (type)
-            {
-                case PrefabType.DisconnectedModelPrefabInstance: return false;
-                case PrefabType.DisconnectedPrefabInstance: return false;
-
-                case PrefabType.MissingPrefabInstance: return false;
-
-                case PrefabType.ModelPrefab: return true;
-                case PrefabType.ModelPrefabInstance: return false;
-
-                case PrefabType.None: return false;
-
-                case PrefabType.Prefab: return true;
-                case PrefabType.PrefabInstance: return false;
-            }
-
-            throw new UnaccountedBranchException("type", type);
+            return false;
         }
 
         static public bool IsPrefabInstance(this UnityEngine.Object item)
         {
-            PrefabType type = PrefabUtility.GetPrefabType(item);
+            if (PrefabUtility.GetPrefabInstanceStatus(item) != PrefabInstanceStatus.NotAPrefab)
+                return true;
 
-            switch (type)
-            {
-                case PrefabType.DisconnectedModelPrefabInstance: return true;
-                case PrefabType.DisconnectedPrefabInstance: return true;
-
-                case PrefabType.MissingPrefabInstance: return true;
-
-                case PrefabType.ModelPrefab: return false;
-                case PrefabType.ModelPrefabInstance: return true;
-
-                case PrefabType.None: return false;
-
-                case PrefabType.Prefab: return false;
-                case PrefabType.PrefabInstance: return true;
-            }
-
-            throw new UnaccountedBranchException("type", type);
+            return false;
         }
     }
 }
