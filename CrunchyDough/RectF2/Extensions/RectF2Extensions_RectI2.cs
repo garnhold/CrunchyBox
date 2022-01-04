9113491b76fbd926e6d45a82beb1d02e0005e046
Garrett Arnhold <garrett@arnholdsystems.com>
2022-01-04 01:25:03 -0800

    Added RectF2 <-> RectI2 Extensions

---
+++ CrunchyDough/RectF2/Extensions/RectF2Extensions_RectI2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Crunchy.Dough
+{    
+    static public class RectF2Extensions_RectI2
+    {
+        static public RectI2 GetRectI2(this RectF2 item)
+        {
+            return RectI2Extensions.CreateLowerLeftRectI2(
+                item.GetLowerLeftPoint().GetVectorI2(), 
+                item.GetSize().GetVectorI2()
+            );
+        }
+    }
+}
---
+++ CrunchyDough/RectI2/Extensions/RectI2Extensions_RectF2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Crunchy.Dough
+{    
+    static public class RectI2Extensions_RectF2
+    {
+        static public RectF2 GetRectF2(this RectI2 item)
+        {
+            return RectF2Extensions.CreateLowerLeftRectF2(
+                item.GetLowerLeftPoint().GetVectorF2(), 
+                item.GetSize().GetVectorF2()
+            );
+        }
+    }
+}
