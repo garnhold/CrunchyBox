5fa2e0cb8516a4ccbd7490910b89d25cfb4dccc1
Garrett Arnhold <garrett@arnholdsystems.com>
2022-01-04 09:51:45 -0800

    Fixed Shrinkwrapping

--- CrunchySandwichBag/Extensions/Sprite/SpriteExtensions_Shrinkwrap.cs
+++ CrunchySandwichBag/Extensions/Sprite/SpriteExtensions_Shrinkwrap.cs
@@ -19,11 +19,17 @@ namespace Crunchy.SandwichBag
             RectI2 original = item.rect.GetRectI2();
             RectI2 shrunk = texture.ShrinkwrapRect(original);
 
-            VectorF2 shrunk_pixel_pivot = shrunk.ConvertRangePointToPercentPoint(item.GetPixelPivot().GetVectorF2());
+            VectorF2 original_pixel_pivot = original.ConvertPercentPointToRangePoint(item.GetNormalizedPivot().GetVectorF2());
+            VectorF2 shrunk_normalized_pivot = shrunk.ConvertRangePointToPercentPoint(original_pixel_pivot);
+
+            Rect new_rect = shrunk.GetRect();
+            Vector2 new_pivot = shrunk_normalized_pivot.GetVector2();
 
             item.ModifySprite(delegate (SerializedProperty property) {
-                property.ForceProperty("m_Pivot").vector2Value = shrunk_pixel_pivot.GetVector2();
-                property.ForceProperty("m_Rect").rectValue = shrunk.GetRect();
+                property.ForceProperty("m_Rect").rectValue = new_rect;
+
+                property.ForceProperty("m_Pivot").vector2Value = new_pivot;
+                property.ForceProperty("m_Alignment").intValue = (int)new_pivot.GetSpriteAlignment();
             });
         }
     }
