712a3de3e82d549c54b42cd660247afff80f5b5c
Garrett Arnhold <garrett@arnholdsystems.com>
2022-01-03 20:08:27 -0800

    Added Texture2D.GetRectI2

---
+++ CrunchySandwich/Extensions/Texture2D/Texture2DExtensions_RectI2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+namespace Crunchy.Sandwich
+{
+    using Dough;
+
+    static public class Texture2DExtensions_RectI2
+    {
+        static public RectI2 GetRectI2(this Texture2D item)
+        {
+            return RectI2Extensions.CreateLowerLeftRectI2(0, 0, item.width, item.height);
+        }
+    }
+}
