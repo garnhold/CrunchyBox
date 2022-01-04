506f729a37e726210b524a395bbc5257bab179fa
Garrett Arnhold <garrett@arnholdsystems.com>
2022-01-03 19:04:29 -0800

    Added Rect.GetAdjusted/Enlarged/Shrunk RectSide Flavors

--- CrunchySandwich/Extensions/Rect/RectExtensions_Adjust_Side.cs
+++ CrunchySandwich/Extensions/Rect/RectExtensions_Adjust_Side.cs
@@ -60,5 +60,42 @@ namespace Crunchy.Sandwich
         {
             return item.GetShrunk(0.0f, 0.0f, 0.0f, amount);
         }
+
+        static public Rect GetAdjusted(this Rect item, float amount, RectSide side)
+        {
+            switch (side)
+            {
+                case RectSide.Left: return item.GetAdjustedLeft(amount);
+                case RectSide.Right: return item.GetAdjustedRight(amount);
+                case RectSide.Bottom: return item.GetAdjustedBottom(amount);
+                case RectSide.Top: return item.GetAdjustedTop(amount);
+            }
+
+            throw new UnaccountedBranchException("side", side);
+        }
+        static public Rect GetEnlarged(this Rect item, float amount, RectSide side)
+        {
+            switch (side)
+            {
+                case RectSide.Left: return item.GetEnlargedLeft(amount);
+                case RectSide.Right: return item.GetEnlargedRight(amount);
+                case RectSide.Bottom: return item.GetEnlargedBottom(amount);
+                case RectSide.Top: return item.GetEnlargedTop(amount);
+            }
+
+            throw new UnaccountedBranchException("side", side);
+        }
+        static public Rect GetShrunk(this Rect item, float amount, RectSide side)
+        {
+            switch (side)
+            {
+                case RectSide.Left: return item.GetShrunkLeft(amount);
+                case RectSide.Right: return item.GetShrunkRight(amount);
+                case RectSide.Bottom: return item.GetShrunkBottom(amount);
+                case RectSide.Top: return item.GetShrunkTop(amount);
+            }
+
+            throw new UnaccountedBranchException("side", side);
+        }
     }
 }
