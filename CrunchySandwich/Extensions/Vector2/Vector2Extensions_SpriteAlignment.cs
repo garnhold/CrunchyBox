35160ab9349b3a4b79b3148fba8b97101ffb4f61
Garrett Arnhold <garrett@arnholdsystems.com>
2022-01-04 09:49:48 -0800

    Added Vector2.GetSpriteAlignment

---
+++ CrunchySandwich/Extensions/Vector2/Vector2Extensions_SpriteAlignment.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+namespace Crunchy.Sandwich
+{
+    using Dough;
+
+    static public class Vector2Extensions_SpriteAlignment
+    {
+        static public SpriteAlignment GetSpriteAlignment(this Vector2 item)
+        {
+            SpriteAlignment alignment;
+
+            if (typeof(SpriteAlignment)
+                .GetEnumValues<SpriteAlignment>()
+                .TryFindFirst(a => a.GetVector2() == item, out alignment))
+                return alignment;
+
+            return SpriteAlignment.Custom;
+        }
+    }
+}
