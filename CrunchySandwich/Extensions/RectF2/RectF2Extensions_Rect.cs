73d8f7704d8eb1868f3167a68e97c9acc8932be2
Garrett Arnhold <garrett@arnholdsystems.com>
2022-01-04 00:17:39 -0800

    Added Some Sandwich RectF2 Extensions

---
+++ CrunchySandwich/Extensions/RectF2/RectF2Extensions_Rect.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+namespace Crunchy.Sandwich
+{
+    using Dough;
+
+    static public class RectF2Extensions_Rect
+    {
+        static public Rect GetRect(this RectF2 item)
+        {
+            return new Rect(
+                item.GetLowerLeftPoint().GetVector2(), 
+                item.GetSize().GetVector2()
+            );
+        }
+    }
+}
