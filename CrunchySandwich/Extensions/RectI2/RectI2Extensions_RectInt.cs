5164f7b1db5e460992772d5bbab11c4ab4a93b36
Garrett Arnhold <garrett@arnholdsystems.com>
2022-01-04 00:22:20 -0800

    Added Some RectI2 Sandwich Extensions

---
+++ CrunchySandwich/Extensions/RectI2/RectI2Extensions_RectInt.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+namespace Crunchy.Sandwich
+{
+    using Dough;
+
+    static public class RectF2Extensions_RectInt
+    {
+        static public RectInt GetIntRect(this RectI2 item)
+        {
+            return new RectInt(
+                item.GetLowerLeftPoint().GetVector2Int(), 
+                item.GetSize().GetVector2Int()
+            );
+        }
+    }
+}
