53f117b75f7e0c57683ef7664d65aed3571ef53a
Garrett Arnhold <garrett@arnholdsystems.com>
2022-01-03 19:56:58 -0800

    Added RectSides

---
+++ CrunchyDough/RectSides/RectSides.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Crunchy.Dough
+{
+    static public class RectSides
+    {
+        static public readonly RectSide[] VALUES = new RectSide[] {
+            RectSide.Left,
+            RectSide.Right,
+            RectSide.Bottom,
+            RectSide.Top
+        };
+    }
+}
