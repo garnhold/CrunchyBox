74b0c24d0ed3eb8f2e408bc043bb53f654e90480
Garrett Arnhold <garrett@arnholdsystems.com>
2022-01-04 00:53:18 -0800

    Simplified Texture2DExtensions_Sprites_Collider w/Texture2D.ModifySprites

--- CrunchySandwichBag/Extensions/Texture2D/Texture2DExtensions_Sprites_Collider.cs
+++ CrunchySandwichBag/Extensions/Texture2D/Texture2DExtensions_Sprites_Collider.cs
@@ -14,19 +14,12 @@ namespace Crunchy.SandwichBag
     {
         static public void GenerateSpriteColliders(this Texture2D item, SpriteVectorizer vectorizer)
         {
-            item.ModifyAssetImporter(delegate(SerializedObject obj) {
-                GetSpriteSerializedPropertys(obj)
-                    .Process(p => GenerateSpriteCollider(item, p, vectorizer));
-            });
+            item.ModifyAllSprites(p => GenerateSpriteCollider(item, p, vectorizer));
         }
 
         static public void GenerateSpriteCollider(this Texture2D item, Sprite sprite, SpriteVectorizer vectorizer)
         {
-            item.ModifyAssetImporter(delegate(SerializedObject obj) {
-                GetSpriteSerializedPropertys(obj)
-                    .Narrow(p => p.GetChildValue<string>("m_SpriteID") == sprite.GetSpriteId())
-                    .Process(p => GenerateSpriteCollider(item, p, vectorizer));
-            });
+            item.ModifySprite(sprite, p => GenerateSpriteCollider(item, p, vectorizer));
         }
 
         static private void GenerateSpriteCollider(Texture2D texture, SerializedProperty property, SpriteVectorizer vectorizer)
@@ -40,19 +33,6 @@ namespace Crunchy.SandwichBag
         static private void GenerateSpriteCollider(Sprite sprite, SerializedProperty property, SpriteVectorizer vectorizer)
         {
             property.SetChildValue("m_PhysicsShape", vectorizer.VectorizeSprite(sprite));
-        }
-        static private IEnumerable<SerializedProperty> GetSpriteSerializedPropertys(SerializedObject obj)
-        {
-            switch (obj.GetChildValue<SpriteImportMode>("m_SpriteMode"))
-            {
-                case SpriteImportMode.Single:
-                    return obj.ForceProperty("m_SpriteSheet").WrapAsEnumerable();
-
-                case SpriteImportMode.Multiple:
-                    return obj.ForceProperty("m_SpriteSheet.m_Sprites").GetArrayElements();
-            }
-
-            return Empty.IEnumerable<SerializedProperty>();
         }
     }
 }
