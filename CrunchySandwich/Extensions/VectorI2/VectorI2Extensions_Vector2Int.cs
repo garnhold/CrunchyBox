2563efd530a686f0c556e1734d82450f853bab63
Garrett Arnhold <garrett@arnholdsystems.com>
2022-01-04 00:21:18 -0800

    Added VectorI2.GetVector2Int()

---
+++ CrunchySandwich/Extensions/VectorI2/VectorI2Extensions_Vector2Int.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+namespace Crunchy.Sandwich
+{
+    using Dough;
+
+    static public class VectorI2Extensions_Vector2Int
+    {
+        static public Vector2Int GetVector2Int(this VectorI2 item)
+        {
+            return new Vector2Int(item.x, item.y);
+        }
+    }
+}
