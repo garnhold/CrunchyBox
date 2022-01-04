dc96dc3a686e176f3070d5aad374e31b42dda681
Garrett Arnhold <garrett@arnholdsystems.com>
2022-01-04 09:47:51 -0800

    Added SpriteAlignment Extension

---
+++ CrunchySandwich/Extensions/SpriteAlignment/SpriteAlignmentExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+namespace Crunchy.Sandwich
+{
+    using Dough;
+
+    static public class SpriteAlignmentExtensions
+    {
+    }
+}
---
+++ CrunchySandwich/Extensions/SpriteAlignment/SpriteAlignmentExtensions_Vector2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+namespace Crunchy.Sandwich
+{
+    using Dough;
+
+    static public class SpriteAlignmentExtensions_Vector2
+    {
+        static public Vector2 GetVector2(this SpriteAlignment item)
+        {
+            switch (item)
+            {
+                case SpriteAlignment.Center: return new Vector2(0.5f, 0.5f);
+
+                case SpriteAlignment.TopLeft: return new Vector2(0.0f, 1.0f);
+                case SpriteAlignment.TopCenter: return new Vector2(0.5f, 1.0f);
+                case SpriteAlignment.TopRight: return new Vector2(1.0f, 1.0f);
+
+                case SpriteAlignment.LeftCenter: return new Vector2(0.0f, 0.5f);
+                case SpriteAlignment.RightCenter: return new Vector2(1.0f, 0.5f);
+
+                case SpriteAlignment.BottomLeft: return new Vector2(0.0f, 0.0f);
+                case SpriteAlignment.BottomCenter: return new Vector2(0.5f, 0.0f);
+                case SpriteAlignment.BottomRight: return new Vector2(1.0f, 0.0f);
+
+                case SpriteAlignment.Custom: return new Vector2(0.5f, 0.5f);
+            }
+
+            throw new UnaccountedBranchException("item", item);
+        }
+    }
+}
