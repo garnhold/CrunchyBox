0cbb86aecf263c66414d7a5068229f0d9ec326aa
Garrett Arnhold <garrett@arnholdsystems.com>
2022-01-06 10:50:22 -0800

    Added MotionController_SequencedSpriteValue

---
+++ CrunchySandwich/MotionController/MotionController_SequencedSpriteValue.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+namespace Crunchy.Sandwich
+{
+    using Dough;
+
+    [AddComponentMenu("Motion/MotionController_SequencedSpriteValue")]
+    public class MotionController_SequencedSpriteValue : MotionController
+    {
+        [SerializeFieldEX]private FloatRange range;
+
+        protected override void UpdateInternal(float value)
+        {
+            this.GetComponent<SequencedSpriteRenderer>().SetSequenceValue(value.ConvertFromOffsetToRange(range));
+        }
+    }
+}
