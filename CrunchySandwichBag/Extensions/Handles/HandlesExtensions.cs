a67769743f7478a340853a5164916a91f1d482d8
Garrett Arnhold <garrett@arnholdsystems.com>
2022-01-06 23:57:26 -0800

    Added HandlesExtensions

---
+++ CrunchySandwichBag/Extensions/Handles/HandlesExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEditor;
+
+namespace Crunchy.SandwichBag
+{
+    using Dough;
+    using Noodle;
+
+    static public partial class HandlesExtensions
+    {
+    }
+}
---
+++ CrunchySandwichBag/Extensions/Handles/HandlesExtensions_Position.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEditor;
+
+namespace Crunchy.SandwichBag
+{
+    using Dough;
+    using Noodle;
+    using Sandwich;
+
+    static public partial class HandlesExtensions
+    {
+        static public Vector3 DoPositionHandle(Vector3 position, Quaternion rotation)
+        {
+            return Handles.DoPositionHandle(position, rotation)
+                .GetAtPrecision(-4);
+        }
+        static public Vector3 DoPositionHandle(Vector3 position)
+        {
+            return DoPositionHandle(position, Quaternion.identity);
+        }
+    }
+}
