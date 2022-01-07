40b2367200bf03c0e3924bf8f403e4388ce49e2a
Garrett Arnhold <garrett@arnholdsystems.com>
2022-01-06 23:46:15 -0800

    Added VectorF2.GetAtPrecision

---
+++ CrunchyDough/VectorF2/Extensions/VectorF2Extensions_Precision.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Crunchy.Dough
+{   
+    static public class VectorF2Extensions_Precision
+    {
+        static public VectorF2 GetAtPrecision(this VectorF2 item, int exponent)
+        {
+            return new VectorF2(
+                item.x.GetAtPrecision(exponent),
+                item.y.GetAtPrecision(exponent)
+            );
+        }
+    }
+}
