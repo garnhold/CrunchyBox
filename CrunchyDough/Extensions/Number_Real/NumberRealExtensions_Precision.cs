ed04ccc45f4de0f41193b63a1a19fb1556c76d5d
Garrett Arnhold <garrett@arnholdsystems.com>
2022-01-06 23:42:52 -0800

    Added Real.GetAtPrecision

---
+++ CrunchyDough/Extensions/Number_Real/NumberRealExtensions_Precision.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Crunchy.Dough
+{
+	static public class NumberRealExtensions_Precision
+    {
+		static public float GetAtPrecision(this float item, int exponent)
+		{
+			return item.GetQuantizedMin((float)Mathq.Pow(10, exponent));
+		}
+		static public double GetAtPrecision(this double item, int exponent)
+		{
+			return item.GetQuantizedMin((double)Mathq.Pow(10, exponent));
+		}
+		static public decimal GetAtPrecision(this decimal item, int exponent)
+		{
+			return item.GetQuantizedMin((decimal)Mathq.Pow(10, exponent));
+		}
+	}
+}
