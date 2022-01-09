840206c1ea9363bbc8878b0a5b964e63342b0a89
Garrett Arnhold <garrett@arnholdsystems.com>
2022-01-08 23:16:06 -0800

    Fancied Up SignalPeriodicTransform

--- CrunchySandwich/Signal/Periodic/SignalPeriodicTransform.cs
+++ CrunchySandwich/Signal/Periodic/SignalPeriodicTransform.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 namespace Crunchy.Sandwich
 {
-    using Dough;    
+    using Dough;
+
     [Serializable]
     public class SignalPeriodicTransform
     {
@@ -13,9 +14,74 @@ namespace Crunchy.Sandwich
         [SerializeFieldEX]private float phase = 0.0f;
         [SerializeFieldEX]private float offset = 0.0f;
 
+        public SignalPeriodicTransform(float a, float f, float p, float o)
+        {
+            amplitude = a;
+            frequency = f;
+            phase = p;
+            offset = o;
+        }
+        public SignalPeriodicTransform() : this(1.0f, 1.0f, 0.0f, 0.0f) { }
+
         public float Apply(float input, Operation<float, float> operation)
         {
             return amplitude * operation(input * frequency + phase) + offset;
+        }
+
+        public float GetAmplitude()
+        {
+            return amplitude;
+        }
+
+        public float GetFrequency()
+        {
+            return frequency;
+        }
+
+        public float GetPhase()
+        {
+            return phase;
+        }
+
+        public float GetOffset()
+        {
+            return offset;
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                int hash = 17;
+
+                hash = hash * 23 + amplitude.GetHashCode();
+                hash = hash * 23 + frequency.GetHashCode();
+                hash = hash * 23 + phase.GetHashCode();
+                hash = hash * 23 + offset.GetHashCode();
+                return hash;
+            }
+        }
+
+        public override bool Equals(object obj)
+        {
+            SignalPeriodicTransform cast;
+
+            if (obj.Convert<SignalPeriodicTransform>(out cast))
+            {
+                if (cast.amplitude == amplitude)
+                {
+                    if (cast.frequency == frequency)
+                    {
+                        if (cast.phase == phase)
+                        {
+                            if (cast.offset == offset)
+                                return true;
+                        }
+                    }
+                }
+            }
+
+            return false;
         }
     }
 }
