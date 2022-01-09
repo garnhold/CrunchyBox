59fafc44bf3b2af95ce270d90e29a9424d2a55f5
Garrett Arnhold <garrett@arnholdsystems.com>
2022-01-09 14:14:44 -0800

    Added CameraExtensions_PerspectiveLength

---
+++ CrunchySandwich/Extensions/Camera/CameraExtensions_PerspectiveLength.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+namespace Crunchy.Sandwich
+{
+    using Dough;    
+    static public class CameraExtensions_PerspectiveLength
+    {
+        static public float GetPerspectiveWorldPerPixel(this Camera item, float distance)
+        {
+            return (TrigDegree.Sin(item.fieldOfView / 2.0f) * distance * 2.0f) / item.GetPixelHeight();
+        }
+
+        static public float ConvertPerspectiveWorldToPixels(this Camera item, float distance, float length)
+        {
+            return length / item.GetPerspectiveWorldPerPixel(distance);
+        }
+
+        static public float ConvertPixelsToPerspectiveWorld(this Camera item, float distance, float length)
+        {
+            return length * item.GetPerspectiveWorldPerPixel(distance);
+        }
+    }
+}
