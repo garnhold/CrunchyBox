a9f23eae956cd70814b299857ba3186a993d1c0a
Garrett Arnhold <garrett@arnholdsystems.com>
2022-01-09 00:03:21 -0800

    Added MotionController_FollowPositio

---
+++ CrunchySandwich/MotionController/MotionController_FollowPosition.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+namespace Crunchy.Sandwich
+{
+    using Dough;
+
+    [AddComponentMenu("Motion/MotionController_FollowPosition")]
+    public class MotionController_FollowPosition : MotionController
+    {
+        [SerializeField]private float speed;
+        [SerializeField]private TimeType time_type;
+
+        [SerializeField]private List<GameObject> targets;
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.red.GetWithAlpha(0.2f);
+            targets.Process(t => Gizmos.DrawSphere(t.GetSpacarPosition(), 0.02f));
+        }
+
+        protected override void UpdateInternal(float value)
+        {
+            targets.GetPercentCapped(value.ConvertFromOffsetToPercent())
+                .IfNotNull(t => this.InterpolateSpacarPosition(
+                    t.GetSpacarPosition(), 
+                    speed * time_type.GetDelta()
+                ));
+        }
+    }
+}
