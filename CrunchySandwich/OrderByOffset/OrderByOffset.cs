513dd53d31c151aea7a6d779c04a3fe3d854c54c
Garrett Arnhold <garrett@arnholdsystems.com>
2022-01-09 23:47:38 -0800

    Added Caching to OrderByOffset

--- CrunchySandwich/OrderByOffset/OrderByOffset.cs
+++ CrunchySandwich/OrderByOffset/OrderByOffset.cs
@@ -11,7 +11,14 @@ namespace Crunchy.Sandwich
     public class OrderByOffset : MonoBehaviour
     {
         [SerializeField]private int offset;
-        [SerializeField] private GameObject offset_origin;
+        [SerializeField]private GameObject offset_origin;
+
+        private ComponentCache_UpwardFromParent<Renderer> upward_renderer;
+
+        private void Start()
+        {
+            upward_renderer = new ComponentCache_UpwardFromParent<Renderer>(this);
+        }
 
         private void Update()
         {
@@ -22,7 +29,7 @@ namespace Crunchy.Sandwich
         {
             return offset_origin.IfNotNull(
                 p => p.GetComponent<Renderer>().GetSortingOrder(),
-                () => this.GetComponentValueUpwardFromParent<Renderer, int>(r => r.GetSortingOrder())
+                () => upward_renderer.Use(r => r.GetSortingOrder())
             );
         }
     }
