ca2d3d42750758b03d74edb23435c25962ff407b
Garrett Arnhold <garrett@arnholdsystems.com>
2022-01-14 00:26:08 -0800

    Added NumberReal.BindMagnitudeBelow/Above

--- CrunchyDough/Extensions/Number_Real/NumberRealExtensions_Binding.cs
+++ CrunchyDough/Extensions/Number_Real/NumberRealExtensions_Binding.cs
@@ -6,6 +6,123 @@ namespace Crunchy.Dough
 {
 	static public class NumberRealExtensions_Binding
     {
+        static public float BindMagnitudeBelow(this float item, float bound)
+        {
+            if(item > 0.0f)
+            {
+                float positive = bound.GetPositive();
+                
+                if(item > positive)
+                    return positive;
+            }
+            else
+            {
+                float negative = bound.GetNegative();
+                
+                if(item < negative)
+                    return negative;
+            }
+            
+            return item;
+        }
+        static public float BindMagnitudeAbove(this float item, float bound)
+        {
+        
+            if(item > 0.0f)
+            {
+                float positive = bound.GetPositive();
+                
+                if(item < positive)
+                    return positive;
+            }
+            else
+            {
+                float negative = bound.GetNegative();
+                
+                if(item > negative)
+                    return negative;
+            }
+            
+            return item;
+        }
+        static public double BindMagnitudeBelow(this double item, double bound)
+        {
+            if(item > 0.0)
+            {
+                double positive = bound.GetPositive();
+                
+                if(item > positive)
+                    return positive;
+            }
+            else
+            {
+                double negative = bound.GetNegative();
+                
+                if(item < negative)
+                    return negative;
+            }
+            
+            return item;
+        }
+        static public double BindMagnitudeAbove(this double item, double bound)
+        {
+        
+            if(item > 0.0)
+            {
+                double positive = bound.GetPositive();
+                
+                if(item < positive)
+                    return positive;
+            }
+            else
+            {
+                double negative = bound.GetNegative();
+                
+                if(item > negative)
+                    return negative;
+            }
+            
+            return item;
+        }
+        static public decimal BindMagnitudeBelow(this decimal item, decimal bound)
+        {
+            if(item > 0.0m)
+            {
+                decimal positive = bound.GetPositive();
+                
+                if(item > positive)
+                    return positive;
+            }
+            else
+            {
+                decimal negative = bound.GetNegative();
+                
+                if(item < negative)
+                    return negative;
+            }
+            
+            return item;
+        }
+        static public decimal BindMagnitudeAbove(this decimal item, decimal bound)
+        {
+        
+            if(item > 0.0m)
+            {
+                decimal positive = bound.GetPositive();
+                
+                if(item < positive)
+                    return positive;
+            }
+            else
+            {
+                decimal negative = bound.GetNegative();
+                
+                if(item > negative)
+                    return negative;
+            }
+            
+            return item;
+        }
 	}
 }
 
