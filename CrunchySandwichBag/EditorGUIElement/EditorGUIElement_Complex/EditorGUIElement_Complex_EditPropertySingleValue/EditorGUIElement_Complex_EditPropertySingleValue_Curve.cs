ac3394877e8b0a688329f6a550fd536a0e4f54ed
Garrett Arnhold <garrett@arnholdsystems.com>
2022-01-13 22:11:50 -0800

    Added an Ok EditorGUIElement_Complex_EditPropertySingleValue_Curve

---
+++ CrunchySandwichBag/EditorGUIElement/EditorGUIElement_Complex/EditorGUIElement_Complex_EditPropertySingleValue/EditorGUIElement_Complex_EditPropertySingleValue_Curve.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Reflection;
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEditor;
+
+namespace Crunchy.SandwichBag
+{
+    using Dough;
+    using Noodle;
+    using Sandwich;
+    
+    [EditorGUIElementForType(typeof(Curve), true)]
+    public class EditorGUIElement_Complex_EditPropertySingleValue_Curve : EditorGUIElement_Complex_EditPropertySingleValue<int>
+    {
+        protected override int PullState()
+        {
+            return 1;
+        }
+
+        protected override EditorGUIElement PushState()
+        {
+            EditTarget target = GetProperty().GetContents();
+            EditorGUIElement_Container_Flow_Multiline container = new EditorGUIElement_Container_Flow_Multiline();
+
+            if (target.IsValid())
+            {
+                EditProperty_Array property = target.ForcePropertyArray("percents");
+
+                container.AddWeightedChild(1.0f, new EditorGUIElement_EditPropertyArray_ArraySize(property));
+                container.AddWeightedChild(1.0f, new EditorGUIElement_EditPropertyArray_FloatSequence(property));
+            }
+            
+            return container;
+        }
+
+        public EditorGUIElement_Complex_EditPropertySingleValue_Curve(EditProperty_Single_Value p) : base(p)
+        {
+        }
+    }
+}
