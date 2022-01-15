56905053dbd720cce71e579154cf08e6a466ceb4
Garrett Arnhold <garrett@arnholdsystems.com>
2022-01-15 10:40:27 -0800

    Updated MethodInfoPass Commented Code

--- CrunchySalt/TypeEX/MethodInfoEX/MethodInfoPass.cs
+++ CrunchySalt/TypeEX/MethodInfoEX/MethodInfoPass.cs
@@ -25,9 +25,9 @@ namespace Crunchy.Salt
         public override ParameterInfo ReturnParameter { get { return GetNativeMethodInfo().ReturnParameter; } }
         public override Type ReturnType { get { return GetNativeMethodInfo().ReturnType; } }
         public override ICustomAttributeProvider ReturnTypeCustomAttributes { get { return GetNativeMethodInfo().ReturnTypeCustomAttributes; } }
-        //public override Delegate CreateDelegate(Type delegateType) { return method_info.CreateDelegate(delegateType); }
-        //public override Delegate CreateDelegate(Type delegateType, object target) { return method_info.CreateDelegate(delegateType, target); }
-        
+        //public override Delegate CreateDelegate(Type delegateType) { return GetNativeMethodInfo().CreateDelegate(delegateType); }
+        //public override Delegate CreateDelegate(Type delegateType, object target) { return GetNativeMethodInfo().CreateDelegate(delegateType, target); }
+
         public override MethodInfo GetBaseDefinition() { return GetNativeMethodInfo().GetBaseDefinition(); }
         public override Type[] GetGenericArguments() { return GetNativeMethodInfo().GetGenericArguments(); }
         public override MethodInfo GetGenericMethodDefinition() { return GetNativeMethodInfo().GetGenericMethodDefinition(); }
@@ -39,11 +39,11 @@ namespace Crunchy.Salt
         public override bool ContainsGenericParameters { get { return GetNativeMethodInfo().ContainsGenericParameters; } }
         public override bool IsGenericMethod { get { return GetNativeMethodInfo().IsGenericMethod; } }
         public override bool IsGenericMethodDefinition { get { return GetNativeMethodInfo().IsGenericMethodDefinition; } }
-        //public override bool IsSecurityCritical { get { return method_info.IsSecurityCritical; } }
-        //public override bool IsSecuritySafeCritical { get { return method_info.IsSecuritySafeCritical; } }
-        //public override bool IsSecurityTransparent { get { return method_info.IsSecurityTransparent; } }
+        //public override bool IsSecurityCritical { get { return GetNativeMethodInfo().IsSecurityCritical; } }
+        //public override bool IsSecuritySafeCritical { get { return GetNativeMethodInfo().IsSecuritySafeCritical; } }
+        //public override bool IsSecurityTransparent { get { return GetNativeMethodInfo().IsSecurityTransparent; } }
         public override RuntimeMethodHandle MethodHandle { get { return GetNativeMethodInfo().MethodHandle; } }
-        //public override MethodImplAttributes MethodImplementationFlags { get { return method_info.MethodImplementationFlags; } }
+        //public override MethodImplAttributes MethodImplementationFlags { get { return GetNativeMethodInfo().MethodImplementationFlags; } }
         public override MethodBody GetMethodBody() { return GetNativeMethodInfo().GetMethodBody(); }
         public override MethodImplAttributes GetMethodImplementationFlags() { return GetNativeMethodInfo().GetMethodImplementationFlags(); }
         public override ParameterInfo[] GetParameters() { return GetNativeMethodInfo().GetParameters(); }
@@ -51,7 +51,7 @@ namespace Crunchy.Salt
         public override object Invoke(object obj, BindingFlags invokeAttr, Binder binder, object[] parameters, CultureInfo culture) { return GetNativeMethodInfo().Invoke(obj, invokeAttr, binder, parameters, culture); }
 
         //MemberInfo
-        //public override IEnumerable<CustomAttributeData> CustomAttributes { get { return method_info.CustomAttributes; } }
+        //public override IEnumerable<CustomAttributeData> CustomAttributes { get { return GetNativeMethodInfo().CustomAttributes; } }
         public override Type DeclaringType { get { return GetNativeMethodInfo().DeclaringType; } }
         public override int MetadataToken { get { return GetNativeMethodInfo().MetadataToken; } }
         public override Module Module { get { return GetNativeMethodInfo().Module; } }
@@ -59,7 +59,7 @@ namespace Crunchy.Salt
         public override Type ReflectedType { get { return GetNativeMethodInfo().ReflectedType; } }
         public override object[] GetCustomAttributes(bool inherit) { return GetNativeMethodInfo().GetCustomAttributes(inherit); }
         public override object[] GetCustomAttributes(Type attributeType, bool inherit) { return GetNativeMethodInfo().GetCustomAttributes(attributeType, inherit); }
-        //public override IList<CustomAttributeData> GetCustomAttributesData() { return method_info.GetCustomAttributesData(); }
+        //public override IList<CustomAttributeData> GetCustomAttributesData() { return GetNativeMethodInfo().GetCustomAttributesData(); }
         public override bool IsDefined(Type attributeType, bool inherit) { return GetNativeMethodInfo().IsDefined(attributeType, inherit); }
     }
 }
