f02871d3ea16ee29ae89acc851158d10a2eafe85
Garrett Arnhold <garrett@arnholdsystems.com>
2022-01-27 15:11:09 -0800

    Added RectF2.ConvertRange/PercentPointToPercent/RangePoint

---
+++ CrunchyDough/RectF2/Extensions/RectF2Extensions_Convert.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Crunchy.Dough
+{    
+    static public class RectF2Extensions_Convert
+    {
+        static public VectorF2 ConvertPercentPointToRangePoint(this RectF2 item, VectorF2 point)
+        {
+            return point.ConvertFromPercentToRange(item.GetHorizontalRange(), item.GetVerticalRange());
+        }
+
+        static public VectorF2 ConvertRangePointToPercentPoint(this RectF2 item, VectorF2 point)
+        {
+            return point.ConvertFromRangeToPercent(item.GetHorizontalRange(), item.GetVerticalRange());
+        }
+    }
+}
