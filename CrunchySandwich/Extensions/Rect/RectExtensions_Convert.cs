fdce57c59397f7ca3809325b9c20cae37849f4d7
Garrett Arnhold <garrett@arnholdsystems.com>
2022-01-27 15:11:21 -0800

    Added Rect.ConvertRange/PercentPointToPercent/RangePoint

---
+++ CrunchySandwich/Extensions/Rect/RectExtensions_Convert.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+namespace Crunchy.Sandwich
+{
+    static public class RectExtensions_Convert
+    {
+        static public Vector2 ConvertPercentPointToRangePoint(this Rect item, Vector2 point)
+        {
+            return point.ConvertFromPercentToRange(item.GetHorizontalRange(), item.GetVerticalRange());
+        }
+
+        static public Vector2 ConvertRangePointToPercentPoint(this Rect item, Vector2 point)
+        {
+            return point.ConvertFromRangeToPercent(item.GetHorizontalRange(), item.GetVerticalRange());
+        }
+    }
+}
