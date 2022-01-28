16b91e6f7ac80ad147c8128d5b5d2d542b295010
Garrett Arnhold <garrett@arnholdsystems.com>
2022-01-28 13:50:13 -0800

    Connected AxisFilter Into Input System

---
+++ CrunchyBread/InputAtom/InputAtom_Axis/Composite/InputAtom_Axis_Filter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Crunchy.Bread
+{
+    using Dough;
+
+    public class InputAtom_Axis_Filter : InputCompositeAtom, InputAtom_Axis
+    {
+        private InputAtom_Axis axis;
+        private AxisFilter filter;
+
+        protected override IEnumerable<InputAtom> GetAtoms()
+        {
+            yield return axis;
+        }
+
+        public InputAtom_Axis_Filter(InputAtom_Axis a, AxisFilter f)
+        {
+            axis = a;
+            filter = f;
+        }
+
+        public float GetRawValue()
+        {
+            return filter.Filter(axis.GetRawValue());
+        }
+    }
+}
---
+++ CrunchyBread/InputAtom/InputAtom_Axis/Extensions/InputAtomAxisExtensions_Filter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Crunchy.Bread
+{
+    using Dough;
+
+    static public class InputAtomAxisExtensions_Filter
+    {
+        static public InputAtom_Axis GetWithFilter(this InputAtom_Axis item, AxisFilter filter)
+        {
+            return item.IfNotNull(i => new InputAtom_Axis_Filter(i, filter));
+        }
+
+        static public InputAtom_Axis GetWithDeadZone(this InputAtom_Axis item, float dead_zone)
+        {
+            return item.GetWithFilter(new AxisFilter_DeadZone(dead_zone));
+        }
+        static public InputAtom_Axis GetInverted(this InputAtom_Axis item)
+        {
+            return item.GetWithFilter(new AxisFilter_Invert());
+        }
+        static public InputAtom_Axis GetWithGeneralFilter(this InputAtom_Axis item, float dead_zone, bool invert, bool adaptive_limit = true, bool center_calibrating = false)
+        {
+            return item.GetWithFilter(AxisFilters.General(dead_zone, invert, adaptive_limit, center_calibrating));
+        }
+    }
+}
