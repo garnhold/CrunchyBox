01d34ec1806d4b5fac1713e3144d3956c5beccfd
Garrett Arnhold <garrett@arnholdsystems.com>
2022-02-03 00:42:24 -0800

    Added CardinalDirection.GetAngleInPercent/Degrees/Radians

---
+++ CrunchyDough/CardinalDirection/Extensions/CardinalDirectionExtensions_Angle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Crunchy.Dough
+{    
+    static public class CardinalDirectionExtensions_Angle
+    {
+        static public float GetAngleInPercent(this CardinalDirection item)
+        {
+            switch (item)
+            {
+                case CardinalDirection.Right: return 0.0f / 4.0f;
+                case CardinalDirection.Up: return 1.0f / 4.0f;
+                case CardinalDirection.Left: return 2.0f / 4.0f;
+                case CardinalDirection.Down: return 3.0f / 4.0f;
+            }
+
+            throw new UnaccountedBranchException("item", item);
+        }
+
+        static public float GetAngleInRadians(this CardinalDirection item)
+        {
+            return item.GetAngleInPercent() * Mathq.FULL_REVOLUTION_RADIANS;
+        }
+
+        static public float GetAngleInDegrees(this CardinalDirection item)
+        {
+            return item.GetAngleInPercent() * Mathq.FULL_REVOLUTION_DEGREES;
+        }
+    }
+}
