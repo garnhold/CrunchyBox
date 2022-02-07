86ced4ffdf803ba4938b3c96899faa53825aab43
Garrett Arnhold <garrett@arnholdsystems.com>
2022-02-06 18:33:57 -0800

    Update SpriteGenerator

--- CrunchySandwichBag/SpriteGenerator/SpriteGenerator.cs
+++ CrunchySandwichBag/SpriteGenerator/SpriteGenerator.cs
@@ -15,41 +15,16 @@ namespace Crunchy.SandwichBag
         [SerializeField] private int frame_width;
         [SerializeField] private int frame_height;
 
-        [SerializeField] private int number_frames;
+        [SerializeField] private float frames_per_second;
         [SerializeFieldEX] private Duration duration;
 
-        protected abstract void GenerateInternal(IEnumerable<IGrid<Color>> frames, float time_step);
-
-        [InspectorAction]
-        private void Generate()
+        public SpriteGeneratorSheet GenerateSheet()
         {
-            float aspect = (float)frame_width / (float)frame_height;
-            int grid_width = (int)(Mathq.Sqrt(number_frames) / aspect);
-            int grid_height = number_frames / grid_width;
-
-            string final_filename = Filename.SetFilenameWithExtension(this.GetAssetPath(), name + ".png");
-
-            Texture2D final_texture = Texture2DExtensions.CreateClear(
-                grid_width * frame_width,
-                grid_height * frame_height
+            return SpriteGeneratorSheet.CreateDuration(
+                new VectorI2(frame_width, frame_height),
+                duration.GetSeconds(),
+                frames_per_second
             );
-
-            IGrid<Color> final_grid = final_texture.CreateColorGrid();
-
-            List<VectorI2> frame_coords = number_frames
-                .RepeatOperationWithIndex(i => new VectorI2((i % grid_width) * frame_width, (i / grid_width) * frame_height))
-                .ToList();
-
-            VectorI2 frame_size = new VectorI2(frame_width, frame_height);
-
-            List<IGrid<Color>> frames = frame_coords
-                .Convert(c => final_grid.BoundSub(c, frame_size))
-                .ToList();
-                
-            GenerateInternal(frames, (duration / number_frames).GetSeconds());
-
-            final_texture.Apply();
-            final_texture.SaveAsPNG(final_filename);
         }
     }
 }
---
+++ CrunchySandwichBag/SpriteGenerator/SpriteGeneratorSheet.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+namespace Crunchy.SandwichBag
+{
+    using Dough;
+    using Sauce;
+    using Sandwich;
+
+    public class SpriteGeneratorSheet
+    {
+        private float time_step;
+        private Texture2D texture;
+        private IGrid<Color> grid;
+
+        private List<VectorI2> sprite_coords;
+        private List<IGrid<Color>> sprite_datas;
+
+        static public SpriteGeneratorSheet CreateNumberFrames(VectorI2 frame_size, int number_frame, float time_step)
+        {
+            return new SpriteGeneratorSheet(frame_size, number_frame, time_step);
+        }
+        static public SpriteGeneratorSheet CreateDuration(VectorI2 frame_size, float seconds, float frames_per_second)
+        {
+            return CreateNumberFrames(frame_size, (int)(seconds * frames_per_second), 1.0f / frames_per_second);
+        }
+
+        private SpriteGeneratorSheet(VectorI2 frame_size, int number_frames, float time_step)
+        {
+            float aspect = (float)frame_size.x / (float)frame_size.y;
+            int grid_width = (int)(Mathq.Sqrt(number_frames) / aspect);
+            int grid_height = number_frames / grid_width;
+
+            this.time_step = time_step;
+            this.texture = Texture2DExtensions.CreateClear(grid_width * frame_size.x, grid_height * frame_size.y);
+            this.grid = this.texture.CreateColorGrid();
+
+            this.sprite_coords = number_frames
+                .RepeatOperationWithIndex(i => new VectorI2(i % grid_width, i / grid_width).GetComponentMultiply(frame_size))
+                .ToList();
+
+            this.sprite_datas = this.sprite_coords
+                .Convert(c => this.grid.BoundSub(c, frame_size))
+                .ToList();
+        }
+
+        public float GetTimeStep()
+        {
+            return time_step;
+        }
+
+        public Texture2D GetTexture()
+        {
+            return texture;
+        }
+
+        public IEnumerable<VectorI2> GetSpriteCoords()
+        {
+            return sprite_coords;
+        }
+
+        public IEnumerable<IGrid<Color>> GetSpriteDatas()
+        {
+            return sprite_datas;
+        }
+    }
+}
