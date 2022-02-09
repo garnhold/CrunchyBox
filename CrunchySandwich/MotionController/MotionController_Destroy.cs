88f27884c420b3e278e57dc0a8914a670f2e71ce
Garrett Arnhold <garrett@arnholdsystems.com>
2022-02-09 00:11:50 -0800

    Added MotionController_Destroy

---
+++ CrunchySandwich/MotionController/MotionController_Destroy.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+namespace Crunchy.Sandwich
+{
+    using Dough;
+
+    [AddComponentMenu("Motion/MotionController_Destroy")]
+    public class MotionController_Destroy : MotionController
+    {
+        [SerializeField] private GameObject target;
+        [SerializeField] private float destroy_threshold;
+
+        protected override void UpdateInternal(float value)
+        {
+            if (value >= destroy_threshold)
+                GetTarget().Destroy();
+        }
+
+        public GameObject GetTarget()
+        {
+            return target.Coalesce(gameObject);
+        }
+    }
+}
