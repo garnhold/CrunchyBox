12259bd2bd81d18a9e282e6e42bfa58aa40d57a4
Garrett Arnhold <garrett@arnholdsystems.com>
2022-02-09 14:43:08 -0800

    Added RectF2Packer + RectF2ExpandingPacker

---
+++ CrunchyDough/RectF2Packer/RectF2ExpandingPacker.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Crunchy.Dough
+{
+    public class RectF2ExpandingPacker<T>
+    {
+        private List<T> objects_to_pack;
+        private IEnumerable<VectorF2> sizes;
+
+        private Operation<VectorF2, T> size_operation;
+
+        public RectF2ExpandingPacker(IEnumerable<VectorF2> s, Operation<VectorF2, T> so)
+        {
+            objects_to_pack = new List<T>();
+            sizes = s;
+
+            size_operation = so;
+        }
+
+        public void Add(T to_pack)
+        {
+            objects_to_pack.Add(to_pack);
+        }
+
+        public RectF2Packer<T> Pack()
+        {
+            double total_area = objects_to_pack
+                .Convert(o => size_operation(o).GetComponentsProduct())
+                .Sum();
+
+            List<T> sorted = objects_to_pack
+                .Sort(o => -size_operation(o).GetComponentsMax())
+                .ToList();
+
+            foreach (VectorF2 size in sizes)
+            {
+                if (size.GetComponentsProduct() >= total_area)
+                {
+                    RectF2Packer<T> packer = new RectF2Packer<T>(size, size_operation);
+
+                    if (sorted.AreAll(o => packer.Pack(o)))
+                        return packer;
+                }
+            }
+
+            return null;
+        }
+    }
+}
---
+++ CrunchyDough/RectF2Packer/RectF2Packer.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Crunchy.Dough
+{
+    public class RectF2Packer<T>
+    {
+        private RectF2 full_rect;
+
+        private List<RectF2> remaining_rects;
+        private Dictionary<T, RectF2> packed_objects;
+
+        private Operation<VectorF2, T> size_operation;
+
+        public RectF2Packer(RectF2 r, Operation<VectorF2, T> so)
+        {
+            full_rect = r;
+
+            remaining_rects = new List<RectF2>();
+            packed_objects = new Dictionary<T, RectF2>();
+
+            size_operation = so;
+
+            remaining_rects.Add(full_rect);
+        }
+
+        public RectF2Packer(VectorF2 size, Operation<VectorF2, T> so) : this(RectF2Extensions.CreateLowerLeftRectF2(VectorF2.ZERO, size), so) { }
+        public RectF2Packer(float width, float height, Operation<VectorF2, T> so) : this(new VectorF2(width, height), so) { }
+
+        public bool Pack(T to_pack)
+        {
+            RectF2 best_rect;
+            double best_rating;
+            int best_rect_index;
+
+            VectorF2 size = size_operation(to_pack);
+
+            if (remaining_rects
+                .Narrow(r => size.IsBoundBelow(r.GetSize()))
+                .TryFindLowestRated(r => (r.GetSize() - size).GetComponentsMax(), out best_rect, out best_rating, out best_rect_index))
+            {
+                RectF2 pack_rect = RectF2Extensions.CreateLowerLeftRectF2(best_rect.GetLowerLeftPoint(), size);
+
+                remaining_rects.RemoveAt(best_rect_index);
+                remaining_rects.AddRange(best_rect.GetSubtraction(pack_rect));
+
+                packed_objects.Add(to_pack, pack_rect);
+                return true;
+            }
+
+            return false;
+        }
+
+        public IEnumerable<KeyValuePair<T, RectF2>> GetPacked()
+        {
+            return packed_objects;
+        }
+    }
+}
