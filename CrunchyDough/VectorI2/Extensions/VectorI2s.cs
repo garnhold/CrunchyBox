9e336c592242c335ea68e4333230c7e4576aca40
Garrett Arnhold <garrett@arnholdsystems.com>
2022-02-10 13:06:08 -0800

    Added VectorI2s

---
+++ CrunchyDough/VectorI2/Extensions/VectorI2s.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Crunchy.Dough
+{    
+    static public class VectorI2s
+    {
+        static public IEnumerable<VectorI2> POTDimensionSequence(int max_power)
+        {
+            int x = 1;
+            int y = 1;
+
+            yield return new VectorI2(x, y);
+
+            for (int i = 0; i < max_power; i++)
+            {
+                x *= 2;
+                yield return new VectorI2(x, y);
+
+                y *= 2;
+                yield return new VectorI2(x, y);
+            }
+        }
+    }
+}
