f518ecdfc63359f2f530d9f19092f239d74519d5
Garrett Arnhold <garrett@arnholdsystems.com>
2022-12-21 21:36:55 -0800

    Added InputDevice_Mouse.GetPositionStick

---
+++ CrunchyBread/InputDevice/InputDevice_Mouse/Extensions/InputDeviceMouseExtensions_Stick.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using OpenTK.Input;
+
+namespace Crunchy.Bread
+{
+    using Dough;
+
+    static public class InputDeviceMouseExtensions_Stick
+    {
+        static public InputAtom_Stick GetPositionStick(this InputDevice_Mouse item)
+        {
+            return new InputAtom_Stick_AxisPair(
+                item.GetHorizontalPosition().GetAsAxis(),
+                item.GetVerticalPosition().GetAsAxis()
+            );
+        }
+    }
+}
