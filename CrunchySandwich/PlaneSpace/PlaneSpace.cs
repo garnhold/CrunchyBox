39d8f4666fc07beaacfbb01e399545934bb31ce3
Garrett Arnhold <garrett@arnholdsystems.com>
2022-12-22 20:53:07 -0800

    Added Implicit Operators for Plane<->PlaneSpace

--- CrunchySandwich/PlaneSpace/PlaneSpace.cs
+++ CrunchySandwich/PlaneSpace/PlaneSpace.cs
@@ -15,6 +15,15 @@ namespace Crunchy.Sandwich
         public readonly Vector3 y;
         public readonly Vector3 origin;
 
+        static public implicit operator PlaneSpace(Plane p)
+        {
+            return new PlaneSpace(p);
+        }
+        static public implicit operator Plane(PlaneSpace p)
+        {
+            return p.plane;
+        }
+
         public PlaneSpace(Plane p)
         {
             plane = p;
