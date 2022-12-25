e46aa997befa9484452c1c9d9e65ffcbc01e4033
Garrett Arnhold <garrett@arnholdsystems.com>
2022-12-24 23:39:11 -0800

    Added 'depth_texture_mode' to RenderEffect

--- CrunchySandwich/RenderEffect/RenderEffect.cs
+++ CrunchySandwich/RenderEffect/RenderEffect.cs
@@ -14,6 +14,23 @@ namespace Crunchy.Sandwich
     public class RenderEffect : MonoBehaviour
     {
         [SerializeField]private Material material;
+        [SerializeField]private DepthTextureMode depth_texture_mode;
+
+        private void TouchDepthTextureMode()
+        {
+            this.GetComponent<Camera>().depthTextureMode = depth_texture_mode;
+        }
+
+        private void Start()
+        {
+            TouchDepthTextureMode();
+        }
+
+        private void Update()
+        {
+            if (Application.isPlaying == false)
+                TouchDepthTextureMode();
+        }
 
         private void OnRenderImage(RenderTexture src, RenderTexture dst)
         {
