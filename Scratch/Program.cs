b231af975ce2e9d2e18e096e25e5b201859043e1
Garrett Arnhold <garrett@arnholdsystems.com>
2022-12-26 00:10:45 -0800

    Changed Scratch

--- Scratch/Program.cs
+++ Scratch/Program.cs
@@ -3,57 +3,48 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Reflection;
 
+using System.Threading;
+using System.Threading.Tasks;
+
 using Crunchy.Dough;
 using Crunchy.Salt;
 using Crunchy.Recipe;
 using Crunchy.Sandwich;
+using Crunchy.Dinner;
 
 namespace Scratch
 {
-    public class Poop
-    {
-        [TyonField] public RouletteWheel<string> wheel;
-        [TyonField] public List<KeyValuePair<string, float>> stuff;
-        [TyonField] public VectorF2 vec;
-        [TyonField] public Poop outside;
-
-        public Poop()
-        {
-        }
-    }
-
     class MainClass
     {
+        static public async Task DoThing()
+        {
+            try
+            {
+                IDictionary<string, string> values = await SystemBrowser.ServeAndProcessForm(1212, writer => {
+                    writer.Write(@"
+<html>
+    <body>
+        <form method=""POST"">
+            <input type=""text"" name=""poop"" />
+            <input type=""submit"" value=""submit"" />
+        </form>
+    </body>
+</html>
+                    ");
+                });
+
+                values.Process(p => Console.WriteLine(p.Key + " = " + p.Value));
+            }
+            catch (Exception ex)
+            {
+                Console.Write(ex);
+            }
+        }
+
         public static void Main(string[] args)
         {
-            string tyon = @"
-Poop {
-    outside = @1;
-}
+            DoThing().Wait();
 
-            ";
-
-            object p = new Poop();
-
-            Poop aa = new Poop();
-
-            Process<object, TyonContext> operation = TyonSettings_General.INSTANCE.CompilePushToSystemObject(tyon, TyonHydrationMode.Permissive);
-
-            TyonContext context = TyonSettings_General.INSTANCE.CreateContext(Enumerable.New<object>(aa));
-
-            Console.WriteLine(operation.GetDynamicMethodILBody());
-
-            operation(p, context);
-
-            string tyon2 = TyonSettings_General.INSTANCE.Serialize(p);
-
-            Console.WriteLine(tyon2);
-
-            object deserialized2 = TyonSettings_General.INSTANCE.Deserialize(tyon2, TyonHydrationMode.Strict);
-
-            string tyon3 = TyonSettings_General.INSTANCE.Serialize(deserialized2);
-
-            Console.WriteLine(tyon3);
             Console.ReadLine();
         }
 
