23df9dce4439eb9ad46da5ff3bca1446ec032626
Garrett Arnhold <garrett@arnholdsystems.com>
2022-12-25 20:57:14 -0800

    Added string.SplitToTuple* Methods

--- CrunchyDough/Extensions/String/StringExtensions_SplitAt.cs
+++ CrunchyDough/Extensions/String/StringExtensions_SplitAt.cs
@@ -9,6 +9,14 @@ namespace Crunchy.Dough
             first = item.SubSection(0, index);
             second = item.Offset(index + gap);
         }
+        static public Tuple<string, string> SplitToTupleAtIndex(this string item, int index, int gap = 0)
+        {
+            string first;
+            string second;
+
+            item.SplitAtIndex(index, out first, out second, gap);
+            return Tuple.New(first, second);
+        }
         static public bool TrySplitAtIndex(this string item, int index, out string first, out string second, int gap = 0)
         {
             if (item.IsIndexValid(index))
@@ -26,6 +34,14 @@ namespace Crunchy.Dough
         {
             item.SplitAtIndex(item.FindIndexOf(sub_string), out first, out second, sub_string.GetLength());
         }
+        static public Tuple<string, string> SplitToTupleAtIndexOf(this string item, string sub_string)
+        {
+            string first;
+            string second;
+
+            item.SplitAtIndexOf(sub_string, out first, out second);
+            return Tuple.New(first, second);
+        }
         static public bool TrySplitAtIndexOf(this string item, string sub_string, out string first, out string second)
         {
             int index;
@@ -41,6 +57,14 @@ namespace Crunchy.Dough
         static public void SplitAtLastIndexOf(this string item, string sub_string, out string first, out string second)
         {
             item.SplitAtIndex(item.FindLastIndexOf(sub_string), out first, out second, sub_string.GetLength());
+        }
+        static public Tuple<string, string> SplitToTupleAtLastIndexOf(this string item, string sub_string)
+        {
+            string first;
+            string second;
+
+            item.SplitAtLastIndexOf(sub_string, out first, out second);
+            return Tuple.New(first, second);
         }
         static public bool TrySplitAtLastIndexOf(this string item, string sub_string, out string first, out string second)
         {
