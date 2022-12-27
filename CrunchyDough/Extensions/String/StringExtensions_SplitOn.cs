f23fc9de0dc4158538aca44469853ff5131b3d81
Garrett Arnhold <garrett@arnholdsystems.com>
2022-12-27 12:09:42 -0800

    Added string.SplitOnPartOut + SplitToTupleOn*

--- CrunchyDough/Extensions/String/StringExtensions_SplitOn.cs
+++ CrunchyDough/Extensions/String/StringExtensions_SplitOn.cs
@@ -10,5 +10,42 @@ namespace Crunchy.Dough
         {
             return item.Split(seperators, StringSplitOptions.None);
         }
+
+        static public void SplitOnPartOut(this string item, out string output1, params string[] seperators)
+        {
+            item.SplitOn(seperators).PartOut(out output1);
+        }
+        static public void SplitOnPartOut(this string item, out string output1, out string output2, params string[] seperators)
+        {
+            item.SplitOn(seperators).PartOut(out output1, out output2);
+        }
+        static public void SplitOnPartOut(this string item, out string output1, out string output2, out string output3, params string[] seperators)
+        {
+            item.SplitOn(seperators).PartOut(out output1, out output2, out output3);
+        }
+        static public void SplitOnPartOut(this string item, out string output1, out string output2, out string output3, out string output4, params string[] seperators)
+        {
+            item.SplitOn(seperators).PartOut(out output1, out output2, out output3, out output4);
+        }
+
+        static public Tuple<string, string, string> SplitToTupleOn3(this string item, params string[] seperators)
+        {
+            string output1;
+            string output2;
+            string output3;
+
+            item.SplitOnPartOut(out output1, out output2, out output3, seperators);
+            return Tuple.New(output1, output2, output3);
+        }
+        static public Tuple<string, string, string, string> SplitToTupleOn4(this string item, params string[] seperators)
+        {
+            string output1;
+            string output2;
+            string output3;
+            string output4;
+
+            item.SplitOnPartOut(out output1, out output2, out output3, out output4, seperators);
+            return Tuple.New(output1, output2, output3, output4);
+        }
     }
 }
