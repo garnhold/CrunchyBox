7dad46c262064c8067635ebfc38956ab1ed103ad
Garrett Arnhold <garrett@arnholdsystems.com>
2023-01-04 20:19:22 -0800

    Added Additional Spacar Parent Methods

--- CrunchySandwich/Extensions/GameObjectComponent/GameObjectComponentExtensions_Transform_Spacar_Parent.cs
+++ CrunchySandwich/Extensions/GameObjectComponent/GameObjectComponentExtensions_Transform_Spacar_Parent.cs
@@ -17,6 +17,24 @@ namespace Crunchy.Sandwich
         {
             return item.transform.GetParentLocalSpacarPosition();
         }
+        
+        static public Vector3 GetParentSpacarRotation(this GameObject item)
+        {
+            return item.transform.GetParentSpacarRotation();
+        }
+        static public Vector3 GetParentLocalSpacarRotation(this GameObject item)
+        {
+            return item.transform.GetParentLocalSpacarRotation();
+        }
+
+        static public Quaternion GetParentSpacarQuaternion(this GameObject item)
+        {
+            return item.transform.GetParentSpacarQuaternion();
+        }
+        static public Quaternion GetParentLocalSpacarQuaternion(this GameObject item)
+        {
+            return item.transform.GetParentLocalSpacarQuaternion();
+        }
 		static public Vector3 GetParentSpacarPosition(this Component item)
         {
             return item.transform.GetParentSpacarPosition();
@@ -24,6 +42,24 @@ namespace Crunchy.Sandwich
         static public Vector3 GetParentLocalSpacarPosition(this Component item)
         {
             return item.transform.GetParentLocalSpacarPosition();
+        }
+        
+        static public Vector3 GetParentSpacarRotation(this Component item)
+        {
+            return item.transform.GetParentSpacarRotation();
+        }
+        static public Vector3 GetParentLocalSpacarRotation(this Component item)
+        {
+            return item.transform.GetParentLocalSpacarRotation();
+        }
+
+        static public Quaternion GetParentSpacarQuaternion(this Component item)
+        {
+            return item.transform.GetParentSpacarQuaternion();
+        }
+        static public Quaternion GetParentLocalSpacarQuaternion(this Component item)
+        {
+            return item.transform.GetParentLocalSpacarQuaternion();
         }
 	}
 }
--- CrunchySandwich/Extensions/Transform/TransformExtensions_Spacar_Parent.cs
+++ CrunchySandwich/Extensions/Transform/TransformExtensions_Spacar_Parent.cs
@@ -18,5 +18,23 @@ namespace Crunchy.Sandwich
         {
             return item.parent.IfNotNull(p => p.GetLocalSpacarPosition(), Vector3.zero);
         }
+
+        static public Vector3 GetParentSpacarRotation(this Transform item)
+        {
+            return item.parent.IfNotNull(p => p.GetSpacarRotation(), Vector3.zero);
+        }
+        static public Vector3 GetParentLocalSpacarRotation(this Transform item)
+        {
+            return item.parent.IfNotNull(p => p.GetLocalSpacarRotation(), Vector3.zero);
+        }
+
+        static public Quaternion GetParentSpacarQuaternion(this Transform item)
+        {
+            return item.parent.IfNotNull(p => p.GetSpacarQuaternion(), Quaternion.identity);
+        }
+        static public Quaternion GetParentLocalSpacarQuaternion(this Transform item)
+        {
+            return item.parent.IfNotNull(p => p.GetLocalSpacarQuaternion(), Quaternion.identity);
+        }
     }
 }
