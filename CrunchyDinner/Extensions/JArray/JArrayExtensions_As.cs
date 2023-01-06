2d4f357b6037f910c751dd49a15a04d2488d1590
Garrett Arnhold <garrett@arnholdsystems.com>
2023-01-05 16:47:54 -0800

    Added JArray.As*

---
+++ CrunchyDinner/Extensions/JArray/JArrayExtensions_As.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+using System.Collections;
+using System.Collections.Generic;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+using Crunchy.Dough;
+
+namespace Crunchy.Dinner
+{
+    static public class JArrayExtensions_As
+    {
+        static public IEnumerable<JObject> AsJObjects(this JArray item)
+        {
+            return item.Convert<JToken, JObject>();
+        }
+
+        static public IEnumerable<JArray> AsJArrays(this JArray item)
+        {
+            return item.Convert<JToken, JArray>();
+        }
+
+        static public IEnumerable<JValue> AsJValues(this JArray item)
+        {
+            return item.Convert<JToken, JValue>();
+        }
+
+        static public IEnumerable<object> AsNatives(this JArray item)
+        {
+            return item.AsJValues().Convert(v => v.Value);
+        }
+
+        static public IEnumerable<string> AsStrings(this JArray item)
+        {
+            return item.AsNatives().Convert(n => n.ToStringEX());
+        }
+    }
+}
