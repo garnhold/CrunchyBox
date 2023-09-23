88bc2abda3c7a98e9803cf9af920c0d890c5063c
Garrett Arnhold <garrett@arnholdsystems.com>
2023-09-22 19:16:09 -0700

    Added IEnumerable<JToken>.ToJArray

---
+++ CrunchyDinner/Extensions/JToken/IEnumerable/JTokenExtensions_IEnumerable.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+using System.Collections;
+using System.Collections.Generic;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+using Crunchy.Dough;
+
+namespace Crunchy.Dinner
+{
+    static public class JTokenExtensions_IEnumerable
+    {
+        static public JArray ToJArray(this IEnumerable<JToken> item)
+        {
+            return new JArray(item);
+            JArray array = new JArray();
+
+            foreach (JToken token in item)
+                array.Add(token);
+
+            return array;
+        }
+    }
+}
---
+++ CrunchyDinner/Extensions/JToken/JTokenExtensions.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+using System.Collections;
+using System.Collections.Generic;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+using Crunchy.Dough;
+
+namespace Crunchy.Dinner
+{
+    static public class JTokenExtensions
+    {
+    }
+}
