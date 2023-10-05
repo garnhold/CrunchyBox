20ad65e9306a30f16734af98a84f21657f2b7a08
Garrett Arnhold <garrett@arnholdsystems.com>
2023-10-04 22:42:28 -0700

    Added Common TokenPatterns

--- CrunchyMenu/Lexer/Token/TokenPattern/TokenPatterns.cs
+++ CrunchyMenu/Lexer/Token/TokenPattern/TokenPatterns.cs
@@ -44,5 +44,47 @@ namespace Crunchy.Menu
         {
             return CharacterSet((IEnumerable<char>)characters);
         }
+
+        static private TokenPattern NUMBER = CharacterRange('0', '9');
+        static public TokenPattern Number()
+        {
+            return NUMBER;
+        }
+
+        static private TokenPattern LOWERCASE = CharacterRange('a', 'z');
+        static public TokenPattern Lowercase()
+        {
+            return LOWERCASE;
+        }
+
+        static private TokenPattern UPPERCASE = CharacterRange('A', 'Z');
+        static public TokenPattern Uppercase()
+        {
+            return UPPERCASE;
+        }
+
+        static private TokenPattern ALPHABETICAL = Multiple(LOWERCASE, UPPERCASE);
+        static public TokenPattern Alphabetical()
+        {
+            return ALPHABETICAL;
+        }
+
+        static private TokenPattern ALPHANUMERIC = Multiple(ALPHABETICAL, NUMBER);
+        static public TokenPattern AlphaNumeric()
+        {
+            return ALPHANUMERIC;
+        }
+
+        static private TokenPattern ALPHABETICAL_AND_UNDERSCORE = Multiple(ALPHABETICAL, Character('_'));
+        static public TokenPattern AlphabeticalAndUnderscore()
+        {
+            return ALPHABETICAL_AND_UNDERSCORE;
+        }
+
+        static private TokenPattern ALPHANUMERIC_AND_UNDERSCORE = Multiple(ALPHANUMERIC, Character('_'));
+        static public TokenPattern AlphaNumericAndUnderscore()
+        {
+            return ALPHANUMERIC_AND_UNDERSCORE;
+        }
     }
 }
