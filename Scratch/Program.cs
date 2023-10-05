72397b1f9cdc5d762a904d7dd98c2210f213edea
Garrett Arnhold <garrett@arnholdsystems.com>
2023-10-04 22:43:01 -0700

    Scratch Poke

--- Scratch/Program.cs
+++ Scratch/Program.cs
@@ -11,39 +11,40 @@ using Crunchy.Salt;
 using Crunchy.Recipe;
 using Crunchy.Sandwich;
 using Crunchy.Dinner;
+using Crunchy.Menu;
 
 namespace Scratch
 {
     class MainClass
     {
-        static public async Task DoThing()
-        {
-            try
-            {
-                IDictionary<string, string> values = await SystemBrowser.ServeAndProcessForm(1212, writer => {
-                    writer.Write(@"
-<html>
-    <body>
-        <form method=""POST"">
-            <input type=""text"" name=""poop"" />
-            <input type=""submit"" value=""submit"" />
-        </form>
-    </body>
-</html>
-                    ");
-                });
-
-                values.Process(p => Console.WriteLine(p.Key + " = " + p.Value));
-            }
-            catch (Exception ex)
-            {
-                Console.Write(ex);
-            }
-        }
-
         public static void Main(string[] args)
         {
-            DoThing().Wait();
+            TokenDefinition token = new TokenDefinition(
+                TokenDefinitionDetectors.Pattern(
+                    TokenPatterns.Sequence(
+                        TokenPatterns.Multiple(
+                            TokenPatterns.CharacterRange('a', 'z'),
+                            TokenPatterns.CharacterRange('A', 'Z'),
+                            TokenPatterns.Character('_')
+                        ).MakeAtleastOne(),
+                        TokenPatterns.Multiple(
+                            TokenPatterns.CharacterRange('a', 'z'),
+                            TokenPatterns.CharacterRange('A', 'Z'),
+                            TokenPatterns.CharacterRange('0', '9'),
+                            TokenPatterns.Character('_')
+                        ).MakeAnyAmount()
+                    )
+                )
+            );
+
+            token = new TokenDefinition(
+                TokenDefinitionDetectors.Pattern(
+                    TokenPatterns.Sequence(
+                        TokenPatterns.AlphabeticalAndUnderscore().MakeAtleastOne(),
+                        TokenPatterns.AlphaNumericAndUnderscore().MakeAnyAmount()
+                    )
+                )
+            );
 
             Console.ReadLine();
         }
