c6dd4949079fd035118c638eee17c1b0b12542a1
Garrett Arnhold <garrett@arnholdsystems.com>
2023-10-05 17:18:07 -0700

    Added Chars

---
+++ CrunchyDough/Numeric/Chars.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Crunchy.Dough
+{
+    static public class Chars
+    {
+        static public IEnumerable<char> Range(char first, char last, bool include_end)
+        {
+            if (include_end)
+            {
+                for (char i = first; i <= last; i++)
+                    yield return i;
+            }
+            else
+            {
+                for (char i = first; i < last; i++)
+                    yield return i;
+            }
+        }
+    }
+}
