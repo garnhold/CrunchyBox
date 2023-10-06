8659d8cfb013d5bf0fb56e8d0992e301f7c704f9
Garrett Arnhold <garrett@arnholdsystems.com>
2023-10-05 17:53:14 -0700

    Added TokenCharacterSetExtensions_Repeated

---
+++ CrunchyMenu/Lexer/Token/TokenPattern/TokenCharacterSet/Extensions/TokenCharacterSetExtensions_Repeated.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using Crunchy.Dough;
+
+namespace Crunchy.Menu
+{
+    static public class TokenCharacterSetExtensions_Repeated
+    {
+        static public TokenPattern MakeRepeated(this TokenCharacterSet item, int min, int max)
+        {
+            return TokenPatterns.Characters(min, max, item);
+        }
+
+        static public TokenPattern MakeOptional(this TokenCharacterSet item)
+        {
+            return TokenPatterns.OptionalCharacter(item);
+        }
+
+        static public TokenPattern ZeroOrMore(this TokenCharacterSet item)
+        {
+            return TokenPatterns.ZeroOrMoreCharacters(item);
+        }
+
+        static public TokenPattern OneOrMore(this TokenCharacterSet item)
+        {
+            return TokenPatterns.OneOrMoreCharacters(item);
+        }
+    }
+}
