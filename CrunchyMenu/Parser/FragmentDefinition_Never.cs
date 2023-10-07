3dbdda020e97a4077dcc0ddbb14e9aae04c24ab7
Garrett Arnhold <garrett@arnholdsystems.com>
2023-10-07 16:00:56 -0700

    Addtional Progress

---
+++ CrunchyMenu/Parser/FragmentDefinition_Never.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using Crunchy.Dough;
+
+namespace Crunchy.Menu
+{
+    public class FragmentDefinition_Never<T> : FragmentDefinition<T>
+    {
+        public static readonly FragmentDefinition_Never<T> INSTANCE = new FragmentDefinition_Never<T>();
+
+        protected override bool ConsumeInternal(IList<TokenInstance> tokens, int index, out int new_index, out Operation<T> producer)
+        {
+            new_index = -1;
+            producer = null;
+            return false;
+        }
+
+        private FragmentDefinition_Never()
+        {
+        }
+
+        public override bool IsRequired()
+        {
+            return true;
+        }
+    }
+}
