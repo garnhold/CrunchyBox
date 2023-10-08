8d2f0cec574ca4b093931527b01c3b4cc92b6823
Garrett Arnhold <garrett@arnholdsystems.com>
2023-10-08 01:03:53 -0700

    Added TokenDefinition.MakeFragment

---
+++ CrunchyMenu/Language/Lexer/Token/Extensions/TokenDefinitionExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using Crunchy.Dough;
+
+namespace Crunchy.Menu
+{
+    static public class TokenDefinitionExtensions
+    {
+    }
+}
---
+++ CrunchyMenu/Language/Lexer/Token/Extensions/TokenDefinitionExtensions_FragmentDefinition.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using Crunchy.Dough;
+
+namespace Crunchy.Menu
+{
+    static public class TokenDefinitionExtensions_FragmentDefinition
+    {
+        static public FragmentDefinition<T> MakeFragment<T>(this TokenDefinition t, Operation<T, string> o)
+        {
+            return FragmentDefinitions.Token(t, o);
+        }
+
+        static public FragmentDefinition<bool> MakeFragment(this TokenDefinition t)
+        {
+            return t.MakeFragment(s => true);
+        }
+    }
+}
