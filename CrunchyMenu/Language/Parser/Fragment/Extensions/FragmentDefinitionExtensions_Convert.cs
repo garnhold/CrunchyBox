e89dd48337978b3c622c33debb4df1b215a137c1
Garrett Arnhold <garrett@arnholdsystems.com>
2023-10-09 20:25:45 -0700

    Added FragmentDefinition.MakeConvert

---
+++ CrunchyMenu/Language/Parser/Fragment/Extensions/FragmentDefinitionExtensions_Convert.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using Crunchy.Dough;
+
+namespace Crunchy.Menu
+{
+    static public class FragmentDefinitionExtensions_Convert
+    {
+        static public FragmentDefinition<T> MakeConvert<T, J>(this FragmentDefinition<J> item, Operation<T, J> operation)
+        {
+            return FragmentDefinitions.Convert(item, operation);
+        }
+    }
+}
