b04a5082ae0bede7d63b1af4c3701c6ef3138c48
Garrett Arnhold <garrett@arnholdsystems.com>
2023-10-10 23:21:31 -0700

    Added AutoToken

---
+++ CrunchyMenu/Language/Lexer/AutoToken/Extensions/TokenModeExtensions_AutoToken.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using Crunchy.Dough;
+
+namespace Crunchy.Menu
+{
+    static public class TokenModeExtensions_AutoToken
+    {
+        static public AutoToken CreateAutoToken(this TokenMode item)
+        {
+            return new AutoToken(item);
+        }
+
+        static public Operation<TokenDefinition, string> CreateTOK(this TokenMode item)
+        {
+            return item.CreateAutoToken().GetTOK();
+        }
+    }
+}
