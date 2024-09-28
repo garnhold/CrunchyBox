7277d508a28046285110c25b681c407de8bf2804
Garrett Arnhold <garrett@arnholdsystems.com>
2024-09-27 22:01:38 -0700

    Added double/float.GetIEEE754Bytes

---
+++ CrunchyDough/Extensions/Double/DoubleExtensions_IEEE754.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Crunchy.Dough
+{    
+    static public class DoubleExtensions_IEEE754
+    {
+        static public byte[] GetIEEE754Bytes(this double item)
+        {
+            return BitConverter.GetBytes(item);
+        }
+    }
+}
---
+++ CrunchyDough/Extensions/Float/FloatExtensions_IEEE754.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Crunchy.Dough
+{    
+    static public class FloatExtensions_IEEE754
+    {
+        static public byte[] GetIEEE754Bytes(this float item)
+        {
+            return BitConverter.GetBytes(item);
+        }
+    }
+}
