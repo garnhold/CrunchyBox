c48e5fcb9bdd3f40a12420c838cfc1ae8f321358
Garrett Arnhold <garrett@arnholdsystems.com>
2026-01-07 23:50:43 -0800

    Added JToken.ToTabbedJson

---
+++ CrunchyDinner/Extensions/JToken/JTokenExtensions_ToTabbedJson.cs
@@ -0,0 +1,31 @@
+using System;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+
+using System.Collections;
+using System.Collections.Generic;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+using Crunchy.Dough;
+
+namespace Crunchy.Dinner
+{
+    static public class JTokenExtensions_ToTabbedJson
+    {
+        static public string ToTabbedJson(this JToken item)
+        {
+            using (StringWriter string_writer = new StringWriter())
+            using (JsonTextWriter json_writer = new JsonTextWriter(string_writer))
+            {
+                json_writer.IndentChar = '\t';
+                json_writer.Indentation = 1;
+
+                item.WriteTo(json_writer);
+                return string_writer.ToString();
+            }
+        }
+    }
+}
